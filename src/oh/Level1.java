b07ddc9e8a9bc33a024ae13ee2e18d0a88a9c215
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-08-03 12:04:37 +0900

    Level1

--- src/oh/Level1.java
+++ src/oh/Level1.java
@@ -3,5 +3,21 @@ package oh;
 public class Level1 {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+		System.out.println(" \\    /\\\r\n"
+				+ " )  ( ')\r\n"
+				+ "(  /  )\r\n"
+				+ " \\(__)|");
+		System.out.println("|\\_/|\r\n"
+				+ "|q p|   /}\r\n"
+				+ "( 0 )\"\"\"\\\r\n"
+				+ "|\"^\"`    |\r\n"
+				+ "||_/=\\\\__|");
+		int a =2;
+		int b =1;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		
 	}
 }
