ccf0f8268ee2ac04b651e035b3d2304f94ad0c5c
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-08-08 14:58:16 +0900

    Level3

---
+++ src/oh/Level3.java
@@ -0,0 +1,13 @@
+package oh;
+
+import java.util.Scanner;
+
+public class Level3 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i=1; i<10; i++){
+			System.out.println(n*i );
+			};
+	}
+}
