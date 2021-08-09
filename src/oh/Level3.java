565b99839596125c7573ae6d39db7b64d5e95e50
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-08-09 10:41:38 +0900

    Level3

--- src/oh/Level3.java
+++ src/oh/Level3.java
@@ -6,8 +6,32 @@ public class Level3 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		for(int i=1; i<10; i++){
-			System.out.println(n*i );
-			};
+//		for(int i=1; i<10; i++){
+//			System.out.println(n*i );
+//			};
+		int arr[] = new int[n];
+		 
+		for (int i = 0; i < n; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			arr[i] = a + b;
+		}
+		sc.close();
+ 
+		for (int k : arr) {
+			System.out.println(k);
+		}
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
 	}
 }
