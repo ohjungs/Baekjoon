ec37ad00c4442eee625746e7b27505172284343a
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-08-10 15:13:03 +0900

    Level3

--- src/oh/Level3.java
+++ src/oh/Level3.java
@@ -9,21 +9,26 @@ public class Level3 {
 //		for(int i=1; i<10; i++){
 //			System.out.println(n*i );
 //			};
-		int arr[] = new int[n];
-		 
-		for (int i = 0; i < n; i++) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			arr[i] = a + b;
+		// 
+//		int arr[] = new int[n];
+//		 
+//		for (int i = 0; i < n; i++) {
+//			int a = sc.nextInt();
+//			int b = sc.nextInt();
+//			arr[i] = a + b;
+//		}
+//		sc.close();
+// 
+//		for (int k : arr) {
+//			System.out.println(k);
+//		}
+		
+		//n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
+		int sum=0;
+		for (int i = 1; i <= n; i++) {
+			sum += i;
 		}
-		sc.close();
- 
-		for (int k : arr) {
-			System.out.println(k);
-		}
-			
-			
-			
+		System.out.println(sum);
 			
 			
 			
