0b5390ecaed3c22240bacbfa83ac43e6eec9e04c
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-08-12 11:35:44 +0900

    Level3

--- src/oh/Level3.java
+++ src/oh/Level3.java
@@ -9,7 +9,7 @@ import java.util.StringTokenizer;
 public class Level3 {
 	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(System.in);
-//		int n = sc.nextInt();
+		int n = sc.nextInt();
 //		for(int i=1; i<10; i++){
 //			System.out.println(n*i );
 //			};
@@ -52,19 +52,27 @@ public class Level3 {
 		bw.close();
 		 */
 			
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		StringBuilder sb = new StringBuilder();
- 
-		for (int i = 0; i < N; i++) {		
-			String str = br.readLine();
-			int target = str.indexOf(" ");
-			int result = Integer.parseInt(str.substring(0,target)) + Integer.parseInt(str.substring(target + 1));		
-			sb.append(result+"\n");
-		}
-		
-		br.close();
-		System.out.print(sb);
-			
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		int N = Integer.parseInt(br.readLine());
+//		StringBuilder sb = new StringBuilder();
+// 
+//		for (int i = 0; i < N; i++) {		
+//			String str = br.readLine();
+//			int target = str.indexOf(" ");
+//			int result = Integer.parseInt(str.substring(0,target)) + Integer.parseInt(str.substring(target + 1));		
+//			sb.append(result+"\n");
+//		}
+//		
+//		br.close();
+//		System.out.print(sb);
+//		자연수 N이 주어졌을 때, 1부터 N까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+//		입력
+//		첫째 줄에 100,000보다 작거나 같은 자연수 N이 주어진다.
+//		출력
+//		첫째 줄부터 N번째 줄 까지 차례대로 출력한다.
+	for(int i=1; i<=n; i++) {
+		System.out.println(i);
+	}
+	
 	}
 }
