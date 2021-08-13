ebadf64b40d3d036959c668d77ea209e7da1b499
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-08-13 09:40:08 +0900

    Level3

--- src/oh/Level3.java
+++ src/oh/Level3.java
@@ -70,9 +70,13 @@ public class Level3 {
 //		첫째 줄에 100,000보다 작거나 같은 자연수 N이 주어진다.
 //		출력
 //		첫째 줄부터 N번째 줄 까지 차례대로 출력한다.
-	for(int i=1; i<=n; i++) {
-		System.out.println(i);
-	}
-	
+//	for(int i=1; i<=n; i++) {
+//		System.out.println(i);
+//		
+//	}
+//		자연수 N이 주어졌을 때, N부터 1까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+		for(int i=1; i<=n; n--) {
+			System.out.println(n);
+		}
 	}
 }
