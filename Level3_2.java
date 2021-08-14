232c37f5b8bd31c9e54dd8c37f7cfb8b84a87d1a
oh <81200313+ohjungs@users.noreply.github.com>
2021-08-14 23:50:43 +0900

    Add files via upload

---
+++ Level3_2.java
@@ -0,0 +1,23 @@
+package oh;
+
+import java.util.Scanner;
+
+public class Level3_2 { 	public static void main(String[] args) {
+	
+		/*
+		 * ¿ 문제 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오. 입력 첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+		 * 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10) 출력 각 테스트 케이스마다
+		 * "Case #x: "를 출력한 다음, A+B를 출력한다. 테스트 케이스 번호는 1부터 시작한다.
+		 */
+	Scanner sc = new Scanner(System.in);
+	int n= sc.nextInt();
+	for(int i=0; i<n; i++) {
+		int num1=sc.nextInt();
+		int num2=sc.nextInt();
+		System.out.println( "Case #"+(i+1) +": " + (num1+num2) );
+
+	};
+				sc.close();
+}
+
+}
