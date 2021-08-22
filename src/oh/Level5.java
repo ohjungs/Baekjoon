f1e9f4960e1a9f6fb148fa2184732289e36560ee
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-08-22 13:22:32 +0900

    Level5

---
+++ src/oh/Level5.java
@@ -0,0 +1,28 @@
+package oh;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Level5 {
+
+	public static void main(String[] args) {
+		/*
+		 * N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
+		 * 입력 첫째 줄에 정수의 개수 N (1 ≤ N ≤ 1,000,000)이 주어진다. 둘째 줄에는 N개의 정수를 공백으로 구분해서 주어진다.
+		 * 모든 정수는 -1,000,000보다 크거나 같고, 1,000,000보다 작거나 같은 정수이다.
+		 * 출력 첫째 줄에 주어진 정수 N개의 최솟값과 최댓값을 공백으로 구분해 출력한다.
+		 */
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		
+		for(int i=0; i<n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		sc.close();
+		Arrays.sort(arr);
+		System.out.println(arr[0] + " " + arr[n-1]);
+	}
+
+}
