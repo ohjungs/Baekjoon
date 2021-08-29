f9e93e2a4da64c0ff124e515bbec77988af09d25
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-08-29 15:11:42 +0900

    Level6

---
+++ src/oh/Level6.java
@@ -0,0 +1,12 @@
+package oh;
+
+
+public class Level6 {
+	 long sum(int[] a) {
+	        long sum = 0;
+	        for(int i  = 0; i < a.length; i++){
+	            sum += a[i];
+	        }
+	        return sum;
+	    }
+}
