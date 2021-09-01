df97cd77abf56461d64cddc0825a048a07978a23
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-09-02 01:39:22 +0900

    Level7-2

---
+++ src/StepByStepAlgorithm/Level7_2.java
@@ -0,0 +1,20 @@
+package StepByStepAlgorithm;
+
+import java.util.Scanner;
+
+public class Level7_2 {
+
+	public static void main(String[] args) {
+		/*
+		 * ¿ 문제 N개의 숫자가 공백 없이 쓰여있다. 이 숫자를 모두 합해서 출력하는 프로그램을 작성하시오. 첫째 줄에 숫자의 개수 N (1 ≤ N
+		 * ≤ 100)이 주어진다. 둘째 줄에 숫자 N개가 공백없이 주어진다. 출력 입력으로 주어진 숫자 N개의 합을 출력한다.
+		 */
+
+		Scanner sc =new Scanner(System.in);
+		String n = sc.next();
+		String n2 = sc.next();
+		System.out.println(n+n2);
+		
+	}
+
+}
