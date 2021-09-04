478ac1b33148358ed4e613158bbb303bc48823c9
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-09-04 11:32:32 +0900

    Level7-3 other

--- src/StepByStepAlgorithm/Level7_3.java
+++ src/StepByStepAlgorithm/Level7_3.java
@@ -1,11 +1,13 @@
 package StepByStepAlgorithm;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Level7_3 {
 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+/*		Scanner in = new Scanner(System.in);
 		 
 		 
 		int[] arr = new int[26];
@@ -29,3 +31,28 @@ public class Level7_3 {
 		}
 	}
 }
+*/
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 
+		int[] arr = new int[26];
+		
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = -1;
+		}
+ 
+		String S = br.readLine();
+ 
+		for(int i = 0; i < S.length(); i++) {
+			char ch = S.charAt(i);
+    
+			if(arr[ch - 'a'] == -1) {	// arr 원소 값이 -1 인 경우에만 초기화
+				arr[ch - 'a'] = i;
+			}
+		}
+ 
+		for(int val : arr) {	// 배열 출력
+			System.out.print(val + " ");
+		}
+	}
+}
+ 
