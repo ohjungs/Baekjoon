fe9ed3be17a29cc962b788d607ed080137fe773e
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-09-13 08:46:08 +0900

    Level7-9

--- src/StepByStepAlgorithm/Level7_9.java
+++ src/StepByStepAlgorithm/Level7_9.java
@@ -1,5 +1,7 @@
 package StepByStepAlgorithm;
 
+import java.util.Scanner;
+
 public class Level7_9 {
 
 	/*
@@ -20,6 +22,80 @@ public class Level7_9 {
 	 * 출력 입력으로 주어진 단어가 몇 개의 크로아티아 알파벳으로 이루어져 있는지 출력한다.
 	 */
 	public static void main(String[] args) {
-		
+Scanner in = new Scanner(System.in);
+        
+		String str = in.nextLine();
+        
+		int count = 0;
+ 
+		for (int i = 0; i < str.length(); i++) {
+ 
+			char ch = str.charAt(i);
+ 
+			if(ch == 'c') {			// 만약 ch 가 c 라면?
+				if(i < str.length() - 1) {
+					if(str.charAt(i + 1) == '=') {		//만약 ch 다음 문자가 '=' 이라면?
+						// i+1 까지가 하나의 문자이므로 다음 문자를 건너 뛰기 위해 1 증가
+						i++;		
+					}
+					else if(str.charAt(i + 1) == '-') {
+						i++;
+					}
+				}
+			}
+		    
+			else if(ch == 'd') {
+				if(i < str.length() - 1) {
+					if(str.charAt(i + 1) == 'z') {
+						if(i < str.length() - 2) {
+							if(str.charAt(i + 2) == '=') {	// dz= 일 경우
+								i += 2;
+							}
+						}
+					}
+		        
+					else if(str.charAt(i + 1) == '-') {	// d- 일 경우
+						i++;
+					}
+				}
+			}
+		    
+			else if(ch == 'l') {
+				if(i < str.length() - 1) {
+					if(str.charAt(i + 1) == 'j') {	// lj 일 경우
+						i++;
+					}
+				}
+			}
+		    
+			else if(ch == 'n') {
+				if(i < str.length() - 1) {
+					if(str.charAt(i + 1) == 'j') {	// nj 일 경우
+						i++;
+					}
+				}
+			}
+ 
+			else if(ch == 's') {
+				if(i < str.length() - 1) {
+					if(str.charAt(i + 1) == '=') {	// s= 일 경우
+						i++;
+					}
+				}
+		    }
+ 
+			else if(ch == 'z') {
+				if(i < str.length() - 1) {
+					if(str.charAt(i + 1) == '=') {	// z= 일 경우
+						i++;
+					}
+				}
+			}
+		    
+			count++;
+ 
+		}
+ 
+		System.out.println(count);
 	}
 }
