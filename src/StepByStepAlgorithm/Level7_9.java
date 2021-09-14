c2f726ab90ead6f4d700ba57e2d4d7f7d2480f2f
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-09-14 16:29:28 +0900

    Level7-9 re;

--- src/StepByStepAlgorithm/Level7_9.java
+++ src/StepByStepAlgorithm/Level7_9.java
@@ -99,3 +99,12 @@ Scanner in = new Scanner(System.in);
 		System.out.println(count);
 	}
 }
+/* 아래와 같은 풀이도 가능하다... 
+ * Scanner sc = new Scanner(System.in); String croa=sc.nextLine().trim();
+ * sc.close(); String[] arr= {"c=","c-","dz=","d-","lj","nj","s=","z="}; int
+ * result=0;
+ * 
+ * for(int i=0;i<arr.length;i++) { if(croa.contains(arr[i])) {
+ * croa=croa.replaceAll(arr[i]," "); } } System.out.println(croa.length()); } }
+ * 
+ */
