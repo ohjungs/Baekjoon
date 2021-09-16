6c24bcee3c55648a5fcc1c163a0f4a43ff8a5a2e
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-09-16 14:35:47 +0900

    Level7-10 re;

--- src/StepByStepAlgorithm/Level7_10.java
+++ src/StepByStepAlgorithm/Level7_10.java
@@ -1,5 +1,10 @@
 package StepByStepAlgorithm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Level7_10 {
 	/*
 	 * 문제 1613번 그룹 단어 체커 그룹 단어란 단어에 존재하는 모든 문자에 대해서, 각 문자가 연속해서 나타나는 경우만을 말한다. 예를
@@ -8,8 +13,35 @@ public class Level7_10 {
 	 * 프로그램을 작성하시오. 입력 첫째 줄에 단어의 개수 N이 들어온다. N은 100보다 작거나 같은 자연수이다. 둘째 줄부터 N개의 줄에
 	 * 단어가 들어온다. 단어는 알파벳 소문자로만 되어있고 중복되지 않으며, 길이는 최대 100이다. 출력 첫째 줄에 그룹 단어의 개수를 출력한다
 	 */
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+/*		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int result = n;
+		StringTokenizer st;
+		for(int i=0; i<n; i++) {
+			st = new StringTokenizer(br.readLine());
+			String s = st.nextToken();
+			int cnt=0;
+			for(char c='a'; c<='z'; c++) {
+				int first = s.indexOf(c);
+				int last = s.lastIndexOf(c);
+				if(first!=last) {
+					for(int j=first+1; j<last; j++) {
+						if(s.charAt(j)!=c) {
+							cnt++;
+							break;
+						}
+					}
+				}
+			}
+			if(cnt!=0) {
+				result--;
+			}
+		}
+	System.out.println(result);
 	}
-
+*/
+		
+		
+}
 }
