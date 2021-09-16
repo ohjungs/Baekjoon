3b06a8d68df154c040fd08f5df5b0b7ef200831b
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-09-16 14:40:44 +0900

    Level7-10 rere;

--- src/StepByStepAlgorithm/Level7_10.java
+++ src/StepByStepAlgorithm/Level7_10.java
@@ -1,9 +1,7 @@
 package StepByStepAlgorithm;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Level7_10 {
 	/*
@@ -13,35 +11,29 @@ public class Level7_10 {
 	 * 프로그램을 작성하시오. 입력 첫째 줄에 단어의 개수 N이 들어온다. N은 100보다 작거나 같은 자연수이다. 둘째 줄부터 N개의 줄에
 	 * 단어가 들어온다. 단어는 알파벳 소문자로만 되어있고 중복되지 않으며, 길이는 최대 100이다. 출력 첫째 줄에 그룹 단어의 개수를 출력한다
 	 */
-	public static void main(String[] args) throws IOException {
-/*		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int result = n;
-		StringTokenizer st;
-		for(int i=0; i<n; i++) {
-			st = new StringTokenizer(br.readLine());
-			String s = st.nextToken();
-			int cnt=0;
-			for(char c='a'; c<='z'; c++) {
-				int first = s.indexOf(c);
-				int last = s.lastIndexOf(c);
-				if(first!=last) {
-					for(int j=first+1; j<last; j++) {
-						if(s.charAt(j)!=c) {
-							cnt++;
-							break;
-						}
-					}
+	public static void main(String[] args) {
+		/*
+		 * BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); int
+		 * n = Integer.parseInt(br.readLine()); int result = n; StringTokenizer st;
+		 * for(int i=0; i<n; i++) { st = new StringTokenizer(br.readLine()); String s =
+		 * st.nextToken(); int cnt=0; for(char c='a'; c<='z'; c++) { int first =
+		 * s.indexOf(c); int last = s.lastIndexOf(c); if(first!=last) { for(int
+		 * j=first+1; j<last; j++) { if(s.charAt(j)!=c) { cnt++; break; } } } }
+		 * if(cnt!=0) { result--; } } System.out.println(result); }
+		 */
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int cnt = a;
+		for (int i = 0; i < a; i++) {
+			String word = sc.next();
+			for (int j = 1; j < word.length(); j++) {
+				if (word.indexOf(word.charAt(j - 1)) > word.indexOf(word.charAt(j))) {
+					cnt--;
+					break;
 				}
 			}
-			if(cnt!=0) {
-				result--;
-			}
 		}
-	System.out.println(result);
+		System.out.println(cnt);
 	}
-*/
-		
-		
 }
-}
+// 다시풀기 안풀림..
