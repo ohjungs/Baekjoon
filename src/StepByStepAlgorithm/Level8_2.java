2b5020508ff73ccbe7279d5f7bde662bf81ca05c
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-09-18 16:56:41 +0900

    Level8-2

--- src/StepByStepAlgorithm/Level8_2.java
+++ src/StepByStepAlgorithm/Level8_2.java
@@ -1,5 +1,7 @@
 package StepByStepAlgorithm;
 
+import java.util.Scanner;
+
 public class Level8_2 {
 	/*
 	 * 2292번 벌집 위의 그림과 같이 육각형으로 이루어진 벌집이 있다. 그림에서 보는 바와 같이 중앙의 방 1부터 시작해서 이웃하는 방에
@@ -11,4 +13,23 @@ public class Level8_2 {
 	 * 
 	 * 출력 입력으로 주어진 방까지 최소 개수의 방을 지나서 갈 때 몇 개의 방을 지나는지 출력한다.
 	 */
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		 
+		int N = in.nextInt();
+		int count = 1; // 겹 수(최소 루트)
+		int range = 2;	// 범위 (최솟값 기준) 
+ 
+		if (N == 1) {
+			System.out.print(1);
+		}
+ 
+		else {
+			while (range <= N) {	// 범위가 N보다 커지기 직전까지 반복 
+				range = range + (6 * count);	// 다음 범위의 최솟값으로 초기화 
+				count++;	// count 1 증가 
+			}
+			System.out.print(count);
+		}
+	}
 }
