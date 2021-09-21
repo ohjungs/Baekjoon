b39af03965b75c9cae1baf7971ebb5e13a1ce85e
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-09-21 13:08:04 +0900

    Level8-4 ;

--- src/StepByStepAlgorithm/Level8_4.java
+++ src/StepByStepAlgorithm/Level8_4.java
@@ -12,6 +12,15 @@ public class Level8_4 {
 	 */
 	public static void main(String[] args) {
 		Scanner sc =new Scanner(System.in);
+		int a= sc.nextInt();
+		int b= sc.nextInt();
+		int v = sc.nextInt();
 		
+		int day = (v-b)/(a-b);
+		if((v-b)%(a-b)!=0) {
+			day++;
+		}
+		System.out.println(day);
+	
 	}
 }
