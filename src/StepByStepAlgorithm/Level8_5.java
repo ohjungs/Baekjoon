56d0a130f4b36ff42ce290cf705f7b9dbc1a847d
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-09-23 15:35:45 +0900

    Level8-5

--- src/StepByStepAlgorithm/Level8_5.java
+++ src/StepByStepAlgorithm/Level8_5.java
@@ -1,5 +1,7 @@
 package StepByStepAlgorithm;
 
+import java.util.Scanner;
+
 public class Level8_5 {
 	/*
 	 * 10250번 ACM 호텔 문제 ACM 호텔 매니저 지우는 손님이 도착하는 대로 빈 방을 배정하고 있다. 고객 설문조사에 따르면 손님들은
@@ -34,6 +36,24 @@ public class Level8_5 {
 	 * 번호를 출력한다.
 	 */	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+			Scanner in = new Scanner(System.in);
+			 
+			int T = in.nextInt();	// 테스트 케이스
+	 
+	 
+			for(int i = 0; i < T; i++) {
+				
+				int H = in.nextInt();
+				int W = in.nextInt(); 	// 쓸모없는 변수다.
+				int N = in.nextInt();
+				
+				if(N % H == 0) {
+					System.out.println((H * 100) + (N / H));
+	 
+				} else {
+					System.out.println(((N % H) * 100) + ((N / H) + 1));
+				}
+			}
 
 	}
 
