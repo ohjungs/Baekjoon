ce5aeb80f8f5de62b09e7d50cad905eb5bc0759f
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-09-25 00:20:48 +0900

    Level8-6 RE;

--- src/StepByStepAlgorithm/Level8_6.java
+++ src/StepByStepAlgorithm/Level8_6.java
@@ -1,5 +1,7 @@
 package StepByStepAlgorithm;
 
+import java.util.Scanner;
+
 public class Level8_6 {
 	/*
 	 * 2775번 부녀회장이 될테야 문제 평소 반상회에 참석하는 것을 좋아하는 주희는 이번 기회에 부녀회장이 되고 싶어 각 층의 사람들을 불러
@@ -19,6 +21,31 @@ public class Level8_6 {
 	 * 제한 1 ≤ k, n ≤ 14
 	 */
 	public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
 		
+		// 아파트 생성 
+		int[][] APT = new int[15][15];
+ 
+		for(int i = 0; i < 15; i++) {
+			APT[i][1] = 1;	// i층 1호
+			APT[0][i] = i;	// 0층 i호
+		}
+ 
+ 
+		for(int i = 1; i < 15; i ++) {	// 1층부터 14층까지
+ 
+			for(int j = 2; j < 15; j++) {	// 2호부터 14호까지
+				APT[i][j] = APT[i][j - 1] + APT[i - 1][j];
+			}
+		}
+		
+		// 테스트 부분 		
+		int T = in.nextInt();
+		
+		for(int i = 0; i < T; i++) {
+			int k = in.nextInt();
+			int n = in.nextInt();
+			System.out.println(APT[k][n]);
+		}
 	}
 }
