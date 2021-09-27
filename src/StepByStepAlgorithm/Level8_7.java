9a831d887a9c658275a658870a5fc350b6a5a5d2
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-09-27 16:10:22 +0900

    Level8-7 Re

--- src/StepByStepAlgorithm/Level8_7.java
+++ src/StepByStepAlgorithm/Level8_7.java
@@ -34,6 +34,15 @@ Scanner in = new Scanner(System.in);
 		else if (N % 5 == 2 || N % 5 == 4) {
 			System.out.println((N / 5) + 2);
 		}
-		
+		/*
+		 * BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); int
+		 * N = Integer.parseInt(br.readLine());
+		 * 
+		 * 
+		 * if (N == 4 || N == 7) { System.out.println(-1); } else if (N % 5 == 0) {
+		 * System.out.println(N / 5); } else if (N % 5 == 1 || N % 5 == 3) {
+		 * System.out.println((N / 5) + 1); } else if (N % 5 == 2 || N % 5 == 4) {
+		 * System.out.println((N / 5) + 2); }
+		 */
 	}
 }
