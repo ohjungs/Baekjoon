d29d2618ec1e867dc06e5e7cc7bb5ed09f749567
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-09-28 21:24:15 +0900

    Level8-8

---
+++ src/StepByStepAlgorithm/Level8_8.java
@@ -0,0 +1,18 @@
+package StepByStepAlgorithm;
+
+import java.util.Scanner;
+
+public class Level8_8 {
+	/*
+	 * 10757번 a+b 큰 수 A+B 시간 제한 메모리 제한 제출 정답 맞은 사람 정답 비율 1 초 256 MB 35988 16099
+	 * 13932 48.746% 문제 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+	 * 
+	 * 입력 첫째 줄에 A와 B가 주어진다. (0 < A,B < 1010000)
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println((a+b));
+	}
+}
