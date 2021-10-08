4ee57d168c03de6b0d0eab7032f3049efdc5e919
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-10-08 18:35:54 +0900

    Level9-5

--- src/StepByStepAlgorithm/Level9_5.java
+++ src/StepByStepAlgorithm/Level9_5.java
@@ -1,5 +1,7 @@
 package StepByStepAlgorithm;
 
+import java.util.Scanner;
+
 public class Level9_5 {
 	/*
 	 * 4948번 베르트랑 공준 문제 베르트랑 공준은 임의의 자연수 n에 대하여, n보다 크고, 2n보다 작거나 같은 소수는 적어도 하나
@@ -18,4 +20,38 @@ public class Level9_5 {
 	 * 
 	 * 출력 각 테스트 케이스에 대해서, n보다 크고, 2n보다 작거나 같은 소수의 개수를 출력한다.
 	 */
+public static boolean[] prime = new boolean[246913];
+    
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+        
+		get_prime();	// 소수를 얻는 메소드 실행
+		
+		while(true) {
+			int n = in.nextInt();
+			if(n == 0) break;	// n 이 0 일경우 종료
+            
+			int count = 0;	// 소수 개수를 셀 변수
+            
+			for(int i = n + 1; i <= 2 * n; i++) {
+				if(!prime[i]) count++;
+			}
+			System.out.println(count);
+		}		
+	}
+	
+	// 소수를 얻는 메소드
+	public static void get_prime() {
+		// 0 과 1 은 소수가 아니므로 ture
+		prime[0] = prime[1] = true;
+		
+		for(int i = 2; i <= Math.sqrt(prime.length); i++) {
+			if(prime[i]) continue;
+			for(int j = i * i; j < prime.length; j += i) {
+				prime[j] = true;
+			}
+		}
+	}
+ 
 }
