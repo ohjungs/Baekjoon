e8c270af4914d5a234e50c1790e6cd9e8a30d297
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-10-17 16:12:40 +0900

    Level9-9

--- src/StepByStepAlgorithm/Level9_9.java
+++ src/StepByStepAlgorithm/Level9_9.java
@@ -42,3 +42,27 @@ public class Level9_9 {
 			}
 	}
 }
+/*
+ * BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ * 
+ * 
+ * while(true) { StringTokenizer st = new StringTokenizer(br.readLine()," ");
+ * 
+ * int x = Integer.parseInt(st.nextToken()); int y =
+ * Integer.parseInt(st.nextToken()); int z = Integer.parseInt(st.nextToken());
+ * 
+ * 
+ * // 0 0 0 을 입력받으면 종료 if(x == 0 && y == 0 && z == 0) break;
+ * 
+ * 
+ * if((x * x + y * y) == z * z) { System.out.println("right"); } else if(x * x
+ * == (y * y + z * z)) { System.out.println("right"); } else if(y * y == (z * z
+ * + x * x)) { System.out.println("right"); } else {
+ * System.out.println("wrong"); }
+ * 
+ * }
+ * 
+ * }
+ * 
+ * }
+ */
