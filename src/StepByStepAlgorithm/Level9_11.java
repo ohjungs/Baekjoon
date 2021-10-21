6f3609488a19282bc9520b866e21a8cefdfdd95e
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-10-21 13:51:30 +0900

    Level9-11

--- src/StepByStepAlgorithm/Level9_11.java
+++ src/StepByStepAlgorithm/Level9_11.java
@@ -1,5 +1,7 @@
 package StepByStepAlgorithm;
 
+import java.util.Scanner;
+
 public class Level9_11 {
 	/*
 	 * 1002번 터렛 문제 조규현과 백승환은 터렛에 근무하는 직원이다. 하지만 워낙 존재감이 없어서 인구수는 차지하지 않는다. 다음은 조규현과
@@ -21,4 +23,62 @@ public class Level9_11 {
 	 * 출력 각 테스트 케이스마다 류재명이 있을 수 있는 위치의 수를 출력한다. 만약 류재명이 있을 수 있는 위치의 개수가 무한대일 경우에는
 	 * -1을 출력한다.
 	 */
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		 
+		 
+		int T = in.nextInt();
+ 
+		while (T-- > 0) {
+ 
+			int x1 = in.nextInt();
+			int y1 = in.nextInt();
+			int r1 = in.nextInt();
+ 
+			int x2 = in.nextInt();
+			int y2 = in.nextInt();
+			int r2 = in.nextInt();
+			
+			System.out.println(tangent_point(x1, y1, r1, x2, y2, r2));
+		}
+ 
+	}
+ 
+ 
+	// 접점 개수 구하는 함수
+	public static int tangent_point(int x1, int y1, int r1, int x2, int y2, int r2) {
+    
+		int distance_pow = (int)(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));	// 중점간 거리 distance의 제곱 
+ 
+ 
+		// case 1 : 중점이 같으면서 반지름도 같을 경우
+		if(x1 == x2 && y1 == y2 && r1 == r2) {
+			return -1;
+		}
+		
+		// case 2-1 : 두 원의 반지름 합보다 중점간 거리가 더 길 때 
+		else if(distance_pow > Math.pow(r1 + r2, 2)) {
+			return 0;
+		}
+ 
+		// case 2-2 : 원 안에 원이 있으나 내접하지 않을 때 
+		else if(distance_pow < Math.pow(r2 - r1, 2)) {
+			return 0;
+		}
+		
+		// case 3-1 : 내접할 때 
+		else if(distance_pow == Math.pow(r2 - r1, 2)) {
+			return 1;
+		}
+        
+		
+		// case 3-2 : 외접할 때 
+		else if(distance_pow == Math.pow(r1 + r2, 2)) {
+			return 1;
+		}
+		
+		else {
+			return 2;
+		}
+	}
 }
