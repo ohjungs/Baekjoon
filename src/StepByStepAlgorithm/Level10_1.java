b7f21998a111ae5c9af028953533b0f62aaad478
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-10-22 14:31:40 +0900

    Level10-1

---
+++ src/StepByStepAlgorithm/Level10_1.java
@@ -0,0 +1,14 @@
+package StepByStepAlgorithm;
+
+public class Level10_1 {
+	/*
+	 * 10872번 팩토리얼! 문제 0보다 크거나 같은 정수 N이 주어진다. 이때, N!을 출력하는 프로그램을 작성하시오.
+	 * 
+	 * 입력 첫째 줄에 정수 N(0 ≤ N ≤ 12)이 주어진다.
+	 * 
+	 * 출력 첫째 줄에 N!을 출력한다.
+	 */
+	public static void main(String[] args) {
+		
+	}
+}
