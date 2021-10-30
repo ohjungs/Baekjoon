bafe00b8bb21fb90ff75a9364db96f50c8f3b15c
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-10-30 16:07:39 +0900

    Level11-1

--- src/StepByStepAlgorithm/Level11_1.java
+++ src/StepByStepAlgorithm/Level11_1.java
@@ -1,5 +1,7 @@
 package StepByStepAlgorithm;
 
+import java.util.Scanner;
+
 public class Level11_1 {
 	/*
 	 * 2798번 블랙잭 문제 카지노에서 제일 인기 있는 게임 블랙잭의 규칙은 상당히 쉽다. 카드의 합이 21을 넘지 않는 한도 내에서, 카드의
@@ -21,5 +23,54 @@ public class Level11_1 {
 	 * 합이 M을 넘지 않는 카드 3장을 찾을 수 있는 경우만 입력으로 주어진다.
 	 * 
 	 * 출력 첫째 줄에 M을 넘지 않으면서 M에 최대한 가까운 카드 3장의 합을 출력한다.
-	 */
+	 */ 
+	public static void main(String[] args) {
+Scanner in = new Scanner(System.in);
+		
+		int N = in.nextInt();
+		int M = in.nextInt();
+ 
+		int[] arr = new int[N];
+ 
+		for (int i = 0; i < N; i++) {
+			arr[i] = in.nextInt();
+		}
+		
+		int result = search(arr, N, M);
+		System.out.println(result);
+	}
+ 
+	
+	// 탐색
+	static int search(int[] arr, int N, int M) {
+		int result = 0;
+ 
+		// 3개를 고르기 때문에 첫번째 카드는 N - 2 까지만 순회
+		for (int i = 0; i < N - 2; i++) {
+ 
+			// 두 번째 카드는 첫 번째 카드 다음부터 N - 1 까지만 순회
+			for (int j = i + 1; j < N - 1; j++) {
+ 
+				// 세 번째 카드는 두 번째 카드 다음부터 N 까지 순회
+				for (int k = j + 1; k < N; k++) {
+					
+					// 3개 카드의 합 변수 temp
+					int temp = arr[i] + arr[j] + arr[k];
+					
+					// M과 세 카드의 합이 같다면 temp return 및 종료 
+					if (M == temp) {	
+						return temp;
+					}
+					
+					// 세 카드의 합이 이전 합보다 크면서 M 보다 작을 경우 result 갱신 
+					if(result < temp && temp < M) {
+						result = temp;
+					}
+				}
+			}
+		}
+		
+		// 모든 순회를 마치면 result 리턴 
+		return result;
+	}
 }
