6e8547506b8f78315d92be762f251774b39970d8
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-11-02 14:42:09 +0900

    Level11-3

--- src/StepByStepAlgorithm/Level11_3.java
+++ src/StepByStepAlgorithm/Level11_3.java
@@ -1,5 +1,7 @@
 package StepByStepAlgorithm;
 
+import java.util.Scanner;
+
 public class Level11_3 {
 	public static void main(String[] args) {
 		/*
@@ -25,5 +27,29 @@ public class Level11_3 {
 		 * 출력 여러분은 입력에 나열된 사람의 덩치 등수를 구해서 그 순서대로 첫 줄에 출력해야 한다. 단, 각 덩치 등수는 공백문자로 분리되어야
 		 * 한다.
 		 */
+		Scanner in = new Scanner(System.in);
+		 
+		int N = in.nextInt();
+ 
+		int[][] arr = new int[N][2];
+ 
+		for(int i = 0; i < N; i++) {
+			arr[i][0] = in.nextInt();	// [i][0] : 몸무게 
+			arr[i][1] = in.nextInt();	// [i][1] : 키 
+		}
+		
+		
+		for(int i = 0; i < N; i++) {
+			int rank = 1;
+			
+			for(int j = 0; j < N; j++) {
+				if(i == j) continue;
+				if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
+					rank++;
+				}
+			}
+ 
+			System.out.print(rank + " ");
+		}
 }
 }
