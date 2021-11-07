8d1fa4d0d8d6ef267641180dda24989ffb679743
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-11-07 11:22:48 +0900

    Level12-1

--- src/StepByStepAlgorithm/Level12_1.java
+++ src/StepByStepAlgorithm/Level12_1.java
@@ -1,5 +1,7 @@
 package StepByStepAlgorithm;
 
+import java.util.Scanner;
+
 public class Level12_1 {
 	/*
 	 * 2750번 수 정렬하기 문제 N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
@@ -10,6 +12,28 @@ public class Level12_1 {
 	 * 출력 첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
 	 */
 	public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
 		
+		int N = in.nextInt();
+		int[] arr = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = in.nextInt();
+		}
+ 
+		// Bubble sort
+		for(int i = 0; i < N - 1; i++) {
+			for(int j = i + 1; j < N; j++) {
+				if(arr[i] > arr[j]) {
+					int temp = arr[j];
+					arr[j] = arr[i];
+					arr[i] = temp;
+				}
+			}
+		}
+		
+		for(int val : arr) {
+			System.out.println(val);
+		}
 	}
 }
