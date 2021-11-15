9c5b9858e38b7d7f9c99b86345a199a81af32df3
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-11-15 17:09:32 +0900

    Level12-6

--- src/StepByStepAlgorithm/Level12_6.java
+++ src/StepByStepAlgorithm/Level12_6.java
@@ -1,5 +1,8 @@
 package StepByStepAlgorithm;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Level12_6 {
 	/*
 	 * 11650번 좌표 정렬하기 문제 2차원 평면 위의 점 N개가 주어진다. 좌표를 x좌표가 증가하는 순으로, x좌표가 같으면 y좌표가 증가하는
@@ -11,6 +14,27 @@ public class Level12_6 {
 	 * 출력 첫째 줄부터 N개의 줄에 점을 정렬한 결과를 출력한다.
 	 */
 public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
 	
+	int N = in.nextInt();
+	
+	int[][] arr = new int[N][2];
+	
+	for(int i = 0; i < N; i++) {
+		arr[i][0] = in.nextInt();
+		arr[i][1] = in.nextInt();
+	}
+	
+	Arrays.sort(arr, (e1, e2) -> {
+		if(e1[0] == e2[0]) {
+			return e1[1] - e2[1];
+		} else {
+			return e1[0] - e2[0];
+		}
+	});
+	
+	for(int i = 0; i < N; i++) {
+		System.out.println(arr[i][0] + " " + arr[i][1]);
+	}
 }
 }
