89ae769fe6017c259b643f5557cea801da309c11
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-11-19 14:09:54 +0900

    Level12-8

--- src/StepByStepAlgorithm/Level12_8.java
+++ src/StepByStepAlgorithm/Level12_8.java
@@ -1,5 +1,9 @@
 package StepByStepAlgorithm;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
 public class Level12_8 {
 	/*
 	 * 1181번 단어 정렬 문제 알파벳 소문자로 이루어진 N개의 단어가 들어오면 아래와 같은 조건에 따라 정렬하는 프로그램을 작성하시오.
@@ -10,6 +14,38 @@ public class Level12_8 {
 	 * 출력 조건에 따라 정렬하여 단어들을 출력한다. 단, 같은 단어가 여러 번 입력된 경우에는 한 번씩만 출력한다.
 	 */
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		 
+		int N = in.nextInt();
+		String[] arr = new String[N];
+ 
+		in.nextLine();	// 개행 버림
+ 
+		for (int i = 0; i < N; i++) {
+			arr[i] = in.nextLine();
+		}
 		
+		Arrays.sort(arr, new Comparator<String>() {
+			public int compare(String s1, String s2) {
+				// 단어 길이가 같을 경우 
+				if (s1.length() == s2.length()) {
+					return s1.compareTo(s2);
+				} 
+				// 그 외의 경우 
+				else {
+					return s1.length() - s2.length();
+				}
+			}
+		});
+ 
+ 
+		System.out.println(arr[0]);
+		
+		for (int i = 1; i < N; i++) {
+			// 중복되지 않는 단어만 출력
+			if (!arr[i].equals(arr[i - 1])) {
+				System.out.println(arr[i]);
+			}
+		}
 	}
 }
