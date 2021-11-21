5490cde39cbf2ade0527c433ed934fe1b212deab
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-11-21 17:06:41 +0900

    Level12-9

--- src/StepByStepAlgorithm/Level12_9.java
+++ src/StepByStepAlgorithm/Level12_9.java
@@ -1,5 +1,9 @@
 package StepByStepAlgorithm;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
 public class Level12_9 {
 	/*
 	 * 10814번 나이순 정렬 시간 제한 메모리 제한 제출 정답 맞힌 사람 정답 비율 3 초 256 MB 58988 25248 19132
@@ -15,6 +19,29 @@ public class Level12_9 {
 	 * 구분해 출력한다.
 	 */
 	public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
 		
+		int N = in.nextInt();
+		String[][] arr = new String[N][2];
+		
+ 
+		for(int i = 0; i < N; i++) {
+			arr[i][0] = in.next();	// 나이
+			arr[i][1] = in.next();	// 이름
+		}
+ 
+		
+		Arrays.sort(arr, new Comparator<String[]>() {
+			// 나이순으로 정렬
+			@Override
+			public int compare(String[] s1, String[] s2) {
+				return Integer.parseInt(s1[0]) - Integer.parseInt(s2[0]);
+			}
+			
+		});
+		
+		for(int i = 0; i < N; i++) {
+			System.out.println(arr[i][0] + " " + arr[i][1]);
+		}
 	}
 }
