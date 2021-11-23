3e2b729879f5ccd6bc67f825f39dad6f46f15f8c
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-11-23 19:53:23 +0900

    Level12-10

--- src/StepByStepAlgorithm/Level12_10.java
+++ src/StepByStepAlgorithm/Level12_10.java
@@ -1,5 +1,10 @@
 package StepByStepAlgorithm;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Level12_10 {
 	/*
 	 * 18870 좌표 압축 문제 수직선 위에 N개의 좌표 X1, X2, ..., XN이 있다. 이 좌표에 좌표 압축을 적용하려고 한다.
@@ -17,6 +22,32 @@ public class Level12_10 {
 	 * 제한 1 ≤ N ≤ 1,000,000 -109 ≤ Xi ≤ 109
 	 */
 	public static void main(String[] args) {
-		
+		  Scanner sc = new Scanner(System.in);
+	        int N = sc.nextInt();
+	        int[] arr = new int[N];
+	        int[] arrClone;
+	        Map<Integer, Integer> map = new HashMap<>();
+	        int count = 0;
+	        StringBuilder sb = new StringBuilder();
+
+	        for (int i = 0; i < arr.length; i++) {
+	            arr[i] = sc.nextInt();
+	        }
+
+	        arrClone = arr.clone();
+
+	        Arrays.sort(arrClone);
+
+	        for (int i = 0; i < arrClone.length; i++) {
+	            if (!map.containsKey(arrClone[i])) {
+	                map.put(arrClone[i], count++);
+	            }
+	        }
+
+	        for (int i = 0; i < arr.length; i++) {
+	            sb.append(map.get(arr[i])).append(" ");
+	        }
+	        
+	        System.out.println(sb);
 	}
 }
