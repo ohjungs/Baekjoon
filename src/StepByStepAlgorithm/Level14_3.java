759360d0883eab1c3abe1d80582394bd9df5e52f
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-11-29 15:53:30 +0900

    Level14-3

--- src/StepByStepAlgorithm/Level14_3.java
+++ src/StepByStepAlgorithm/Level14_3.java
@@ -1,5 +1,7 @@
 package StepByStepAlgorithm;
 
+import java.util.Scanner;
+
 public class Level14_3 {
 	/*
 	 * 15651번 N과 M(3) 문제 자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을
@@ -13,4 +15,38 @@ public class Level14_3 {
 	 * 
 	 * 수열은 사전 순으로 증가하는 순서로 출력해야 한다.
 	 */
+
+		public static int[] arr;
+		public static int N, M;
+		public static StringBuilder sb = new StringBuilder();
+	 
+		public static void main(String[] args) {
+	 
+			Scanner in = new Scanner(System.in);
+	 
+			N = in.nextInt();
+			M = in.nextInt();
+	 
+			arr = new int[M];
+	        
+			dfs(0);
+			System.out.print(sb);
+	 
+		}
+	 
+		public static void dfs(int depth) {
+			// 깊이가 M이랑 같아지면 출력후 return
+			if (depth == M) {
+				for (int i = 0; i < M; i++) {
+					sb.append(arr[i] + " ");
+				}
+				sb.append('\n');
+				return;
+			}
+	 
+			for (int i = 1; i <= N; i++) {
+				arr[depth] = i;
+				dfs(depth + 1);
+			}
+	}
 }
