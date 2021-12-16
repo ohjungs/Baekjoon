968d755a953877c4b73ec8bef795a9c8cd80e694
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-12-16 17:41:47 +0900

    Level15-3

--- src/StepByStepAlgorithm/Level15_3.java
+++ src/StepByStepAlgorithm/Level15_3.java
@@ -1,5 +1,7 @@
 package StepByStepAlgorithm;
 
+import java.util.Scanner;
+
 public class Level15_3 {
 	/*
 	 * 1904번 01타일 문제 지원이에게 2진 수열을 가르쳐 주기 위해, 지원이 아버지는 그에게 타일들을 선물해주셨다. 그리고 이 각각의
@@ -18,7 +20,31 @@ public class Level15_3 {
 	 * 
 	 * 출력 첫 번째 줄에 지원이가 만들 수 있는 길이가 N인 모든 2진 수열의 개수를 15746으로 나눈 나머지를 출력한다.
 	 */
+	public static int[] dp = new int[1000001];;
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
 		
+		int N = in.nextInt();
+		
+		
+		dp[0] = 0;
+		dp[1] = 1;
+		dp[2] = 2;
+ 
+		// -1 로 초기화
+		for(int i = 3; i < dp.length; i++) {
+			dp[i] = -1;
+		}
+		
+		System.out.println(Tile(N));
+		
+	}
+	
+	public static int Tile(int N) {
+		
+		if(dp[N] == -1) {
+			dp[N] = (Tile(N - 1) + Tile((N - 2))) % 15746;
+		}
+		return dp[N];
 	}
 }
