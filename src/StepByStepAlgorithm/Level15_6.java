b9efd18cfa82ad8b88af53c0396c34669e143dfc
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-12-22 11:35:49 +0900

    Level15-6

--- src/StepByStepAlgorithm/Level15_6.java
+++ src/StepByStepAlgorithm/Level15_6.java
@@ -1,5 +1,7 @@
 package StepByStepAlgorithm;
 
+import java.util.Scanner;
+
 public class Level15_6 {
 	/*
 	 * 1932 정수 삼각형 문제 7 3 8 8 1 0 2 7 4 4 4 5 2 6 5 위 그림은 크기가 5인 정수 삼각형의 한 모습이다.
@@ -13,4 +15,46 @@ public class Level15_6 {
 	 * 
 	 * 출력 첫째 줄에 합이 최대가 되는 경로에 있는 수의 합을 출력한다.
 	 */
+	static int[][] arr;
+	static Integer[][] dp;
+	static int N;
+ 
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+ 
+		N = in.nextInt();
+ 
+		arr = new int[N][N];
+		dp = new Integer[N][N];
+ 
+        
+		// 배열 초기화
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < i + 1; j++) {
+				arr[i][j] = in.nextInt();
+			}
+		}
+		
+		// 가장 마지막 행의 값들을 DP의 마지막 행에도 똑같이 복사
+		for (int i = 0; i < N; i++) {
+			dp[N - 1][i] = arr[N - 1][i];
+		}
+ 
+		System.out.println(find(0, 0));
+ 
+	}
+ 
+	
+	static int find(int depth, int idx) {
+		// 마지막 행일 경우 현재 위치의 dp값 반환
+		if(depth == N - 1) return dp[depth][idx];
+ 
+		// 탐색하지 않았던 값일 경우 다음 행의 양쪽 값 비교
+		if (dp[depth][idx] == null) {
+			dp[depth][idx] = Math.max(find(depth + 1, idx), find(depth + 1, idx + 1)) + arr[depth][idx];
+		}
+		return dp[depth][idx];
+ 
+	}
 }
