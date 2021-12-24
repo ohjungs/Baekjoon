a086404e6c027d116ea741613e3af84af2d1ce76
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-12-24 23:41:17 +0900

    Level15-7

--- src/StepByStepAlgorithm/Level15_7.java
+++ src/StepByStepAlgorithm/Level15_7.java
@@ -1,5 +1,7 @@
 package StepByStepAlgorithm;
 
+import java.util.Scanner;
+
 public class Level15_7 {
 	/*
 	 * 2579번 계단오르기 문제 계단 오르기 게임은 계단 아래 시작점부터 계단 꼭대기에 위치한 도착점까지 가는 게임이다. <그림 1>과 같이
@@ -21,6 +23,39 @@ public class Level15_7 {
 	 * 
 	 * 출력 첫째 줄에 계단 오르기 게임에서 얻을 수 있는 총 점수의 최댓값을 출력한다.
 	 */
-
+	static Integer dp[];
+	static int arr[];
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int N = in.nextInt();
+		
+		dp = new Integer[N + 1];
+		arr = new int[N + 1];
+		
+		for(int i = 1; i <= N; i++) {
+			arr[i] = in.nextInt();
+		}
+		
+		dp[0] = arr[0];	// 디폴트값이 null이므로 0으로 초기화 해주어야한다.
+		dp[1] = arr[1];
+		
+		if(N >= 2) {
+			dp[2] = arr[1] + arr[2];
+		}
+		
+		System.out.println(find(N));
+		
+	}
+	
+	static int find(int N) {
+		// 아직 탐색하지 않는 N번째 계단일 경우
+		if(dp[N] == null) {
+			dp[N] = Math.max(find(N - 2), find(N - 3) + arr[N - 1]) + arr[N];
+		}
+		
+		return dp[N];
+	}
 
 }
