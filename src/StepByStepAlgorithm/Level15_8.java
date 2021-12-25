5db6ea650727043d00593624ab05749b7a2a49a0
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-12-26 00:10:34 +0900

    Level15-8

--- src/StepByStepAlgorithm/Level15_8.java
+++ src/StepByStepAlgorithm/Level15_8.java
@@ -1,5 +1,7 @@
 package StepByStepAlgorithm;
 
+import java.util.Scanner;
+
 public class Level15_8 {
 	/*
 	 * 1463번 1로 만들기 문제 정수 X에 사용할 수 있는 연산은 다음과 같이 세 가지 이다.
@@ -11,4 +13,43 @@ public class Level15_8 {
 	 * 
 	 * 출력 첫째 줄에 연산을 하는 횟수의 최솟값을 출력한다.
 	 */
+
+	static Integer[] dp;
+ 
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+ 
+		int N = in.nextInt();
+ 
+		dp = new Integer[N + 1];
+		dp[0] = dp[1] = 0;
+ 
+		System.out.print(recur(N));
+ 
+	}
+ 
+	static int recur(int N) {
+ 
+		if (dp[N] == null) {
+			// 6으로 나눠지는 경우 
+			if (N % 6 == 0) {
+				dp[N] = Math.min(recur(N - 1), Math.min(recur(N / 3), recur(N / 2))) + 1;
+			}
+			// 3으로만 나눠지는 경우 
+			else if (N % 3 == 0) {
+				dp[N] = Math.min(recur(N / 3), recur(N - 1)) + 1;
+			}
+			// 2로만 나눠지는 경우 
+			else if (N % 2 == 0) {
+				dp[N] = Math.min(recur(N / 2), recur(N - 1)) + 1;
+			}
+			// 2와 3으로 나누어지지 않는 경우
+			else {
+				dp[N] = recur(N - 1) + 1;
+			}
+		}
+		return dp[N];
+	}
+ 
 }
