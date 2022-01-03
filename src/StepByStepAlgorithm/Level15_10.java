63d8ea1cf05d10bf83ace96bfca51b7773629036
ohjungs <81200313+ohjungs@users.noreply.github.com>
2022-01-03 23:25:19 +0900

    Level15-10

--- src/StepByStepAlgorithm/Level15_10.java
+++ src/StepByStepAlgorithm/Level15_10.java
@@ -1,8 +1,6 @@
 package StepByStepAlgorithm;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Level15_10 {
 	/*
@@ -22,39 +20,27 @@ public class Level15_10 {
 	 * 
 	 * 출력 첫째 줄에 최대로 마실 수 있는 포도주의 양을 출력한다.
 	 */
-	static Integer[] dp;
-	static int[] arr;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		dp = new Integer[N + 1];
-		arr = new int[N + 1];
-		
-		for(int i = 1; i < N + 1; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
+
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+ 
+		int N = in.nextInt();
+ 
+		int[] arr = new int[N + 1];
+		int[] dp = new int[N + 1];
+ 
+		for (int i = 1; i <= N; i++) {
+			arr[i] = in.nextInt();
 		}
-		
-		dp[0] = 0;
+ 
 		dp[1] = arr[1];
-		
-		/*
-		 *  (N이 1로 주어질 수 있으므로 이럴 때를 위해 조건식을 달아둔다.
-		 *  또한 dp[2]는 어떤 경우에도 첫 번째와 두 번째를 합한 것이 최댓값이다. 
-		 */
-		if(N > 1) {
+		if (N > 1) {
 			dp[2] = arr[1] + arr[2];
 		}
-		
-		System.out.println(recur(N));
-	}
-	
-	static int recur(int N) {
-		
-		if(dp[N] == null) {
-			dp[N] = Math.max(Math.max(recur(N - 2), recur(N - 3) + arr[N - 1]) + arr[N], recur(N - 1));
+		for (int i = 3; i <= N; i++) {
+			dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i]));
+ 
 		}
-		
-		return dp[N];
+		System.out.println(dp[N]);
 	}}
