70b589ac2062f67ea47ee650716f439cc286a6ee
ohjungs <81200313+ohjungs@users.noreply.github.com>
2022-01-08 03:14:07 +0900

    Level15-12

--- src/StepByStepAlgorithm/Level15_12.java
+++ src/StepByStepAlgorithm/Level15_12.java
@@ -1,5 +1,7 @@
 package StepByStepAlgorithm;
 
+import java.util.Scanner;
+
 public class Level15_12 {
 	/*
 	 * 11054 가장 긴 바이토닉 부분 수열 문제 수열 S가 어떤 수 Sk를 기준으로 S1 < S2 < ... Sk-1 < Sk > Sk+1 >
@@ -15,4 +17,69 @@ public class Level15_12 {
 	 * 
 	 * 출력 첫째 줄에 수열 A의 부분 수열 중에서 가장 긴 바이토닉 수열의 길이를 출력한다.
 	 */
+	static Integer[] r_dp;
+	static Integer[] l_dp;
+	static int[] seq;
+ 
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+ 
+		int N = in.nextInt();
+        
+		r_dp = new Integer[N];	// LIS dp
+		l_dp = new Integer[N];	// LDS dp
+		seq = new int[N];
+ 
+ 
+		for (int i = 0; i < N; i++) {
+			seq[i] = in.nextInt();
+		}
+ 
+		for (int i = 0; i < N; i++) {
+			LIS(i);
+			LDS(i);
+		}
+ 
+		int max = -1;
+		for (int i = 0; i < N; i++) {
+			max = Math.max(r_dp[i] + l_dp[i], max);
+		}
+		System.out.println(max - 1);
+ 
+	}
+ 
+	static int LIS(int N) {
+ 
+		// 만약 탐색하지 않던 위치의 경우
+		if (r_dp[N] == null) {
+			r_dp[N] = 1; // 1로 초기화
+ 
+			// N 이전의 노드들을 탐색
+			for (int i = N - 1; i >= 0; i--) {
+				// 이전의 노드 중 seq[N]의 값보다 작은 걸 발견했을 경우
+				if (seq[i] < seq[N]) {
+					r_dp[N] = Math.max(r_dp[N], LIS(i) + 1);
+				}
+			}
+		}
+		return r_dp[N];
+	}
+ 
+	static int LDS(int N) {
+		
+		// 만약 탐색하지 않던 위치의 경우
+		if (l_dp[N] == null) {
+			l_dp[N] = 1; // 1로 초기화
+ 
+			// N 이후의 노드들을 탐색
+			for (int i = N + 1; i < l_dp.length; i++) {
+				// 이후의 노드 중 seq[N]의 값보다 작은 걸 발견했을 경우
+				if (seq[i] < seq[N]) {
+					l_dp[N] = Math.max(l_dp[N], LDS(i) + 1);
+				}
+			}
+		}
+		return l_dp[N];
+	}
 }
