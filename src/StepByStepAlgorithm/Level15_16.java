a93129b5b861a9c8f1ece7f9dae7f291a571f7e3
ohjungs <81200313+ohjungs@users.noreply.github.com>
2022-01-29 22:35:46 +0900

    Level15-16

--- src/StepByStepAlgorithm/Level15_16.java
+++ src/StepByStepAlgorithm/Level15_16.java
@@ -1,5 +1,10 @@
 package StepByStepAlgorithm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Level15_16 {
 	/*
 	 * 12865번 평번한 배낭 문제 이 문제는 아주 평범한 배낭에 관한 문제이다. 한 달 후면 국가의 부름을 받게 되는 준서는 여행을 가려고
@@ -11,4 +16,52 @@ public class Level15_16 {
 	 * 100,000)와 해당 물건의 가치 V(0 ≤ V ≤ 1,000)가 주어진다. 입력으로 주어지는 모든 수는 정수이다. 출력 한 줄에 배낭에
 	 * 넣을 수 있는 물건들의 가치합의 최댓값을 출력한다.
 	 */
+	static Integer[][] dp;
+	static int[] W; // weight
+	static int[] V; // value
+ 
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+ 
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+ 
+		W = new int[N];
+		V = new int[N];
+ 
+		dp = new Integer[N][K + 1];
+ 
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			W[i] = Integer.parseInt(st.nextToken());
+			V[i] = Integer.parseInt(st.nextToken());
+		}
+ 
+		System.out.println(knapsack(N - 1, K));
+ 
+	}
+ 
+	static int knapsack(int i, int k) {
+		// i가 0미만, 즉 범위 밖이 된다면
+		if (i < 0)
+			return 0;
+		
+		// 탐색하지 않은 위치라면?
+		if (dp[i][k] == null) {
+			
+			// 현재 물건(i)을 추가로 못담는 경우 (이전 i값 탐색) 
+			if(W[i] > k) {
+				dp[i][k] = knapsack(i - 1, k);
+			}
+			// 현재 물건(i)을 담을 수 있는 경우 
+			else {
+				// 이전 i값과 이전 i값에 대한 k-W[i]의 값 + 현재 가치(V[i])중 큰 값을 저장  
+				dp[i][k] = Math.max(knapsack(i - 1, k), knapsack(i - 1, k - W[i]) + V[i]);
+			}
+		}
+		return dp[i][k];
+	}
 }
