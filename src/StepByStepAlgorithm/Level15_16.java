3638b0d81315ee437b9c5c53d90c21afb20909ac
ohjungs <81200313+ohjungs@users.noreply.github.com>
2022-01-30 13:52:55 +0900

    Level15-16

--- src/StepByStepAlgorithm/Level15_16.java
+++ src/StepByStepAlgorithm/Level15_16.java
@@ -16,52 +16,39 @@ public class Level15_16 {
 	 * 100,000)와 해당 물건의 가치 V(0 ≤ V ≤ 1,000)가 주어진다. 입력으로 주어지는 모든 수는 정수이다. 출력 한 줄에 배낭에
 	 * 넣을 수 있는 물건들의 가치합의 최댓값을 출력한다.
 	 */
-	static Integer[][] dp;
-	static int[] W; // weight
-	static int[] V; // value
- 
 	public static void main(String[] args) throws IOException {
- 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
+		
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
- 
+		
+		
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
  
-		W = new int[N];
-		V = new int[N];
+		int[] W = new int[N + 1]; // 무게
+		int[] V = new int[N + 1]; // 가치
+		int[][] dp = new int[N + 1][K + 1];
  
-		dp = new Integer[N][K + 1];
- 
-		for (int i = 0; i < N; i++) {
+		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			W[i] = Integer.parseInt(st.nextToken());
 			V[i] = Integer.parseInt(st.nextToken());
 		}
  
-		System.out.println(knapsack(N - 1, K));
- 
-	}
- 
-	static int knapsack(int i, int k) {
-		// i가 0미만, 즉 범위 밖이 된다면
-		if (i < 0)
-			return 0;
-		
-		// 탐색하지 않은 위치라면?
-		if (dp[i][k] == null) {
-			
-			// 현재 물건(i)을 추가로 못담는 경우 (이전 i값 탐색) 
-			if(W[i] > k) {
-				dp[i][k] = knapsack(i - 1, k);
-			}
-			// 현재 물건(i)을 담을 수 있는 경우 
-			else {
-				// 이전 i값과 이전 i값에 대한 k-W[i]의 값 + 현재 가치(V[i])중 큰 값을 저장  
-				dp[i][k] = Math.max(knapsack(i - 1, k), knapsack(i - 1, k - W[i]) + V[i]);
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= K; j++) {
+				
+				// i번째 무게를 더 담을 수 없는 경우 
+				if(W[i] > j) {
+					dp[i][j] = dp[i - 1][j];
+				}
+				// i번째 무게를 더 담을 수 있는 경우 
+				else {
+					dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - W[i]] + V[i]);
+				}
+				
 			}
 		}
-		return dp[i][k];
+		System.out.println(dp[N][K]);
 	}
 }
