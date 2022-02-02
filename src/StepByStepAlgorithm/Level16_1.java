50109966dce90e6ec3ac89c5c68c088877bf6246
ohjungs <81200313+ohjungs@users.noreply.github.com>
2022-02-03 00:34:57 +0900

    Level16-1

--- src/StepByStepAlgorithm/Level16_1.java
+++ src/StepByStepAlgorithm/Level16_1.java
@@ -1,5 +1,7 @@
 package StepByStepAlgorithm;
 
+import java.util.Scanner;
+
 public class Level16_1 {
 	/*
 	 * 11047번 동전 0 문제 준규가 가지고 있는 동전은 총 N종류이고, 각각의 동전을 매우 많이 가지고 있다. 동전을 적절히 사용해서 그
@@ -9,5 +11,30 @@ public class Level16_1 {
 	 * 최솟값을 출력한다.
 	 */
 
-
+	public static void main(String[] args) {
+		 
+		Scanner in = new Scanner(System.in);
+		
+		int N = in.nextInt();
+		int K = in.nextInt();
+		
+		int[] coin = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			coin[i] = in.nextInt();
+		}
+		
+		int count = 0;
+ 
+		for(int i = N - 1; i >= 0; i--) {
+ 
+			// 현재 동전의 가치가 K보다 작거나 같아야지 구성가능하다.
+			if(coin[i] <= K) {
+				// 현재 가치의 동전으로 구성할 수 있는 개수를 더해준다.
+				count += (K / coin[i]);
+				K = K % coin[i];
+			}
+		}
+		System.out.println(count);
+	}
 }
