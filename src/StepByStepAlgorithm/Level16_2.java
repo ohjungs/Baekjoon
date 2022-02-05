2195784d7ebf6444816763f5420b0da6ce6a8105
ohjungs <81200313+ohjungs@users.noreply.github.com>
2022-02-06 00:00:06 +0900

    Level16-2

--- src/StepByStepAlgorithm/Level16_2.java
+++ src/StepByStepAlgorithm/Level16_2.java
@@ -1,5 +1,9 @@
 package StepByStepAlgorithm;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
 public class Level16_2 {
 	/*
 	 * 1931 회의실 배정 문제 한 개의 회의실이 있는데 이를 사용하고자 하는 N개의 회의에 대하여 회의실 사용표를 만들려고 한다. 각 회의
@@ -10,4 +14,55 @@ public class Level16_2 {
 	 * 공백을 사이에 두고 회의의 시작시간과 끝나는 시간이 주어진다. 시작 시간과 끝나는 시간은 231-1보다 작거나 같은 자연수 또는 0이다.
 	 * 출력 첫째 줄에 최대 사용할 수 있는 회의의 최대 개수를 출력한다.
 	 */
+
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+		
+		int N = in.nextInt();
+		
+		
+		/*
+		  time[][0] 은 시작시점을 의미 
+		  time[][1] 은 종료시점을 의미 
+		*/
+		int[][] time = new int[N][2];
+		
+		
+		for(int i = 0; i < N; i++) {
+			time[i][0] = in.nextInt();	// 시작시간 
+			time[i][1] = in.nextInt();	// 종료시간 
+		}
+		
+		
+		// 끝나는 시간을 기준으로 정렬하기 위해 compare 재정의 
+		Arrays.sort(time, new Comparator<int[]>() {
+			
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				
+				// 종료시간이 같을 경우 시작시간이 빠른순으로 정렬해야한다.  
+				if(o1[1] == o2[1]) {
+					return o1[0] - o2[0];
+				}
+				
+				return o1[1] - o2[1];
+			}
+ 
+		});
+		
+		int count = 0;
+		int prev_end_time = 0;
+		
+		for(int i = 0; i < N; i++) {
+			
+			// 직전 종료시간이 다음 회의 시작 시간보다 작거나 같다면 갱신 
+			if(prev_end_time <= time[i][0]) {
+				prev_end_time = time[i][1];
+				count++;
+			}
+		}
+		
+		System.out.println(count);
+	}
 }
