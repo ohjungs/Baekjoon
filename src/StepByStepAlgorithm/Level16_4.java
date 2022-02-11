28a96fd4ad2d03452c6cc8fff2b154f4807dbd86
ohjungs <place222@naver.com>
2022-02-12 00:53:01 +0900

    Level16-4

--- src/StepByStepAlgorithm/Level16_4.java
+++ src/StepByStepAlgorithm/Level16_4.java
@@ -1,5 +1,7 @@
 package StepByStepAlgorithm;
 
+import java.util.Scanner;
+
 public class Level16_4 {
 //    1541 문제 잃어버린 괄호
 // 세준이는 양수와 +, -, 그리고 괄호를 가지고 식을 만들었다. 그리고 나서 세준이는 괄호를 모두 지웠다.
@@ -13,4 +15,33 @@ public class Level16_4 {
 
 // 출력
 // 첫째 줄에 정답을 출력한다.
+
+public static void main(String[] args) {
+ 
+    Scanner in = new Scanner(System.in);
+
+    int sum = Integer.MAX_VALUE;	// 초기 상태 여부 확인을 위한 값으로 설정 
+    String[] subtraction = in.nextLine().split("-");
+    
+
+    for(int i = 0; i < subtraction.length; i++) {
+        int temp = 0;
+
+        // 뺄셈으로 나뉜 토큰을 덧셈으로 분리하여 해당 토큰들을 더한다.
+        String[] addition = subtraction[i].split("\\+");
+        
+        // 덧셈으로 나뉜 토큰들을 모두 더한다. 
+        for(int j = 0; j < addition.length; j++) {
+            temp += Integer.parseInt(addition[j]);
+        }
+        
+        // 첫 번째토큰인 경우 temp값이 첫 번째 수가 됨
+        if (sum == Integer.MAX_VALUE) {
+            sum = temp;
+        } else {
+            sum -= temp;
+        }
+    }
+    System.out.println(sum);
+}
 }
