514f5e3555861848620d4a220a5531e8a7958785
ohjungs <place222@naver.com>
2022-03-01 23:40:02 +0900

    Level17-3

--- src/StepByStepAlgorithm/Level17_3.java
+++ src/StepByStepAlgorithm/Level17_3.java
@@ -1,5 +1,7 @@
 package StepByStepAlgorithm;
 
+import java.util.Scanner;
+
 public class Level17_3 {
     // 2609번 최대공약수와 최소공배수 문제
     // 두 개의 자연수를 입력받아 최대 공약수와 최소 공배수를 출력하는 프로그램을 작성하시오.
@@ -9,4 +11,27 @@ public class Level17_3 {
 
     // 출력
     // 첫째 줄에는 입력으로 주어진 두 수의 최대공약수를, 둘째 줄에는 입력으로 주어진 두 수의 최소 공배수를 출력한다.
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        int d = gcd(a, b); // 최대공약수
+
+        System.out.println(d);
+        System.out.println(a * b / d);
+
+    }
+
+    // 최대공약수 재귀 방식
+    public static int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+
+        // GCD(a, b) = GCD(b, r)이므로 (r = a % b)
+        return gcd(b, a % b);
+    }
 }
