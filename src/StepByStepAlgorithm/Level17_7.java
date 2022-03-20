271c127bce5c6ccac47cb8d531eb48684f849ad4
ohjungs <place222@naver.com>
2022-03-20 13:36:41 +0900

    Level17-7

--- src/StepByStepAlgorithm/Level17_7.java
+++ src/StepByStepAlgorithm/Level17_7.java
@@ -1,5 +1,10 @@
 package StepByStepAlgorithm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Level17_7 {
     // 11050 이항 계수 문제
     // 자연수 \(N\)과 정수 \(K\)가 주어졌을 때 이항 계수
@@ -11,4 +16,24 @@ public class Level17_7 {
     // 출력
 
     // \(\binom{N}{K}\)를 출력한다.
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        // nCk = n! / ((n-k)! * k!)
+        System.out.println(factorial(N) / (factorial(N - K) * factorial(K)));
+    }
+
+    static int factorial(int N) {
+        // factorial(0) == 1 이다.
+        if (N <= 1) {
+            return 1;
+        }
+        return N * factorial(N - 1);
+    }
 }
