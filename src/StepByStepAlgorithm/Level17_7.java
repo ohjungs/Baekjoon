465229686a4dbf974a5bb8c0ffd7c16d5cc0107c
ohjungs <place222@naver.com>
2022-03-21 16:54:04 +0900

    Level17-7

--- src/StepByStepAlgorithm/Level17_7.java
+++ src/StepByStepAlgorithm/Level17_7.java
@@ -16,6 +16,8 @@ public class Level17_7 {
     // 출력
 
     // \(\binom{N}{K}\)를 출력한다.
+    static int[][] dp;
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,15 +27,25 @@ public class Level17_7 {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        // nCk = n! / ((n-k)! * k!)
-        System.out.println(factorial(N) / (factorial(N - K) * factorial(K)));
+        dp = new int[N + 1][K + 1];
+
+        System.out.println(BC(N, K));
+
     }
 
-    static int factorial(int N) {
-        // factorial(0) == 1 이다.
-        if (N <= 1) {
-            return 1;
+    static int BC(int n, int k) {
+
+        // 이미 풀었던 sub문제일 경우 값을 재활용
+        if (dp[n][k] > 0) {
+            return dp[n][k];
         }
-        return N * factorial(N - 1);
+
+        // 2번 성질
+        if (k == 0 || n == k) {
+            return dp[n][k] = 1;
+        }
+
+        // 1번 성질
+        return dp[n][k] = BC(n - 1, k - 1) + BC(n - 1, k);
     }
 }
