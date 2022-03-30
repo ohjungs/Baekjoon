b639eeddde3598a779fa9bcc83a72f070985f74a
ohjungs <place222@naver.com>
2022-03-31 00:03:42 +0900

    Level17-1

--- src/StepByStepAlgorithm/Level17__1.java
+++ src/StepByStepAlgorithm/Level17__1.java
@@ -1,5 +1,7 @@
 package StepByStepAlgorithm;
 
+import java.util.Scanner;
+
 public class Level17__1 {
     // 스택 10828 문제
     // 정수를 저장하는 스택을 구현한 다음, 입력으로 주어지는 명령을 처리하는 프로그램을 작성하시오.
@@ -17,4 +19,82 @@ public class Level17__1 {
 
     // 출력
     // 출력해야하는 명령이 주어질 때마다, 한 줄에 하나씩 출력한다.
+    public static int[] stack;
+    public static int size = 0;
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int N = in.nextInt();
+
+        stack = new int[N];
+
+        for (int i = 0; i < N; i++) {
+
+            String str = in.next();
+
+            switch (str) {
+
+                case "push":
+                    push(in.nextInt());
+                    break;
+
+                case "pop":
+                    sb.append(pop()).append('\n');
+                    break;
+
+                case "size":
+                    sb.append(size()).append('\n');
+                    break;
+
+                case "empty":
+                    sb.append(empty()).append('\n');
+                    break;
+
+                case "top":
+                    sb.append(top()).append('\n');
+                    break;
+            }
+
+        }
+        System.out.println(sb);
+    }
+
+    public static void push(int item) {
+        stack[size] = item;
+        size++;
+    }
+
+    public static int pop() {
+        if (size == 0) {
+            return -1;
+        } else {
+            int res = stack[size - 1];
+            stack[size - 1] = 0;
+            size--;
+            return res;
+        }
+    }
+
+    public static int size() {
+        return size;
+    }
+
+    public static int empty() {
+        if (size == 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public static int top() {
+        if (size == 0) {
+            return -1;
+        } else {
+            return stack[size - 1];
+        }
+    }
 }
