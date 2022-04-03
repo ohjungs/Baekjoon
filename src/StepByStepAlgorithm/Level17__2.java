a96ff376621457130e511b8ba02c5cf984845ced
ohjungs <place222@naver.com>
2022-04-04 01:15:58 +0900

    Level17-2

--- src/StepByStepAlgorithm/Level17__2.java
+++ src/StepByStepAlgorithm/Level17__2.java
@@ -1,5 +1,8 @@
 package StepByStepAlgorithm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -24,15 +27,15 @@ public class Level17__2 {
     // 출력
     // 재민이가 최종적으로 적어 낸 수의 합을 출력한다. 최종적으로 적어낸 수의 합은 231-1보다 작거나 같은 정수이다.
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         Stack<Integer> stack = new Stack<Integer>();
 
-        int K = in.nextInt();
+        int K = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < K; i++) {
-            int number = in.nextInt();
+            int number = Integer.parseInt(br.readLine()); // 정수 입력
 
             if (number == 0) { // 0이라면 스택에 저장된 top 원소를 지운다.
                 stack.pop();
