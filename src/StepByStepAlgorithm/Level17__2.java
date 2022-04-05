cad99be73f5d790d09b2035bfd0855b426d31f86
ohjungs <place222@naver.com>
2022-04-06 03:51:13 +0900

    Level17-2

--- src/StepByStepAlgorithm/Level17__2.java
+++ src/StepByStepAlgorithm/Level17__2.java
@@ -27,30 +27,32 @@ public class Level17__2 {
     // 출력
     // 재민이가 최종적으로 적어 낸 수의 합을 출력한다. 최종적으로 적어낸 수의 합은 231-1보다 작거나 같은 정수이다.
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-
-        int top = -1; // 마지막 원소의 위치를 가리키는 변수
-
-        int K = in.nextInt();
-        int[] arr = new int[K];
-
-        for (int i = 0; i < K; i++) {
-            int number = in.nextInt(); // 정수 입력
-
-            if (number == 0) { // 0 이라면 top 위치에 있는 원소를 0으로 초기화
-                top--; // top이 가리키는 위치 1 감소
-            } else {
-                top++; // top이 가리키는 위치 1 증가
-                arr[top] = number; // 입력받은 정수로 초기화
-            }
-        }
-        int sum = 0;
-        for (int i = 0; i <= top; i++) { // 합을 구할 때 K가 아닌 top까지다.
-            sum += arr[i];
-        }
-        System.out.println(sum);
-
-    }
-
+   public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		int top = -1;	// 마지막 원소의 위치를 가리키는 변수
+ 
+		int K = Integer.parseInt(br.readLine());
+		int[] arr = new int[K];
+		
+ 
+		for(int i = 0; i < K; i++) {
+			
+			int number = Integer.parseInt(br.readLine());	// 정수 입력
+			
+			if (number == 0) {	// 0 이라면 top 위치에 있는 원소를 0으로 초기화 
+				top--;	// top이 가리키는 위치 1 감소
+			} 
+			else {
+				top++;	// top이 가리키는 위치 1 증가
+				arr[top] = number;	// 입력받은 정수로 초기화
+			}
+		}
+		int sum = 0;
+		for (int i = 0; i <= top; i++) {	// 합을 구할 때 K가 아닌 top까지이다.
+			sum += arr[i];
+		}
+		System.out.println(sum);
+ 
+	}
 }
