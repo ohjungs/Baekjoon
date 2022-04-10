4fd7c49adae42a42b313f5f153f8c5fc9aa83b45
ohjungs <place222@naver.com>
2022-04-10 23:37:39 +0900

    Level17-3

--- src/StepByStepAlgorithm/Level17__3.java
+++ src/StepByStepAlgorithm/Level17__3.java
@@ -24,50 +24,47 @@ public class Level17__3 {
     // 출력은 표준 출력을 사용한다. 만일 입력 괄호 문자열이 올바른 괄호 문자열(VPS)이면 “YES”, 아니면 “NO”를 한 줄에 하나씩
     // 차례대로 출력해야 한다.
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < T; i++) {
+        while (T-- > 0) {
             sb.append(solve(br.readLine())).append('\n');
         }
-
         System.out.println(sb);
+
     }
 
     public static String solve(String s) {
 
-        Stack<Character> stack = new Stack<>();
+        int count = 0;
 
-        for (int i = 0; i < s.length(); i++) {
+        for (char c : s.toCharArray()) {
 
-            char c = s.charAt(i);
-
-            // 여는 괄호일 경우 스택에 넣는다.
+            // 여는 괄호일 경우 카운트 증가
             if (c == '(') {
-                stack.push(c);
+                count++;
             }
 
             // 아래는 모두 닫는 괄호 일 경우들이다.
 
-            // 스택이 비어있는 경우. 즉, 닫는 괄호를 입력받았으나 pop할 원소가 없을 경우
-            else if (stack.empty()) {
+            // count 가 0인 경우. 즉, 닫는 괄호를 입력받았으나 pop할 원소가 없을 경우
+            else if (count == 0) {
                 return "NO";
             }
-            // 그 외의 경우 stack 원소를 pop 한다.
+            // 그 외의 경우 count를 감소시킨다.
             else {
-                stack.pop();
+                count--;
             }
         }
 
         /*
-         * 모든 검사 마치고 스택에 잔여 요소가 있으면 여는 괄호가 많은 경우는 "NO"
-         * 스택이 비어있으면 온전한 수식이므로 "YES" 이다.
+         * 모든 검사 마치고 잔여 요소가 있으면(count > 0) 여는 괄호가 많은 경우는 "NO"
+         * 요소가 비어있으면(count = 0) 온전한 수식이므로 "YES" 이다.
          */
 
-        if (stack.empty()) {
+        if (count == 0) {
             return "YES";
         } else {
             return "NO";
