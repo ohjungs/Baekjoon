2421030ae9226bf470f3a7c7c26d9a2512da8f64
ohjungs <place222@naver.com>
2022-04-17 22:25:53 +0900

    Level17-5

--- src/StepByStepAlgorithm/Level17__5.java
+++ src/StepByStepAlgorithm/Level17__5.java
@@ -23,42 +23,42 @@ public class Level17__5 {
     // 출력
     // 입력된 수열을 만들기 위해 필요한 연산을 한 줄에 한 개씩 출력한다. push연산은 +로, pop 연산은 -로 표현하도록 한다. 불가능한
     // 경우 NO를 출력한다.
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder(); // 출력할 결과물 저장
 
-        Stack<Integer> stack = new Stack<>();
+        int N = in.nextInt();
 
-        int N = Integer.parseInt(br.readLine());
+        int[] stack = new int[N];
 
+        int idx = 0;
         int start = 0;
 
         // N 번 반복
         while (N-- > 0) {
-
-            int value = Integer.parseInt(br.readLine());
+            int value = in.nextInt();
 
             if (value > start) {
                 // start + 1부터 입력받은 value 까지 push를 한다.
                 for (int i = start + 1; i <= value; i++) {
-                    stack.push(i);
-                    sb.append('+').append('\n'); // + 를 저장한다.
+                    stack[idx] = i;
+                    idx++;
+                    sb.append('+').append('\n');
                 }
                 start = value; // 다음 push 할 때의 오름차순을 유지하기 위한 변수 초기화
             }
 
             // top에 있는 원소가 입력받은 값과 같이 않은 경우
-            else if (stack.peek() != value) {
+            else if (stack[idx - 1] != value) {
                 System.out.println("NO");
-                return; // 또는 System.exit(0); 으로 대체해도 됨.
+                System.exit(0); // return 으로 대체해도 됨
             }
 
-            stack.pop();
+            idx--;
             sb.append('-').append('\n');
-
         }
-
         System.out.println(sb);
     }
+
 }
