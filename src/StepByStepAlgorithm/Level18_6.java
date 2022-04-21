d7dac67ae305c2c15f776280d0ee66230aafb884
ohjungs <place222@naver.com>
2022-04-21 16:48:45 +0900

    Level18-6

--- src/StepByStepAlgorithm/Level18_6.java
+++ src/StepByStepAlgorithm/Level18_6.java
@@ -21,41 +21,42 @@ public class Level18_6 {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        Stack<Integer> stack = new Stack<Integer>();
 
         int N = in.nextInt();
         int[] seq = new int[N];
+        int[] stack = new int[N]; // 스택처럼 쓸 배열
+        int top = -1; // 스택의 맨 위 원소를 가리키는 변수
 
         for (int i = 0; i < N; i++) {
             seq[i] = in.nextInt();
         }
 
         for (int i = 0; i < N; i++) {
-
             /*
-             * 스택이 비어있지 않으면서
+             * 스택이 비어있지 않으면서 (= top 이 -1이 아닐 경우)
              * 현재 원소가 스택의 맨 위 원소가 가리키는 원소보다 큰 경우
              * 해당 조건을 만족할 때 까지 stack의 원소를 pop하면서
              * 해당 인덱스의 값을 현재 원소로 바꿔준다.
              */
-            while (!stack.isEmpty() && seq[stack.peek()] < seq[i]) {
-                seq[stack.pop()] = seq[i];
+            while (top != -1 && seq[stack[top]] < seq[i]) {
+                seq[stack[top]] = seq[i];
+                top--;
             }
-
-            stack.push(i);
+            top++;
+            stack[top] = i;
         }
 
         /*
          * 스택의 모든 원소를 pop하면서 해당 인덱스의 value를
          * -1로 초기화한다.
          */
-        while (!stack.isEmpty()) {
-            seq[stack.pop()] = -1;
+        for (int i = top; i >= 0; i--) {
+            seq[stack[i]] = -1;
         }
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            sb.append(seq[i]).append(' ');
+        for (int v : seq) {
+            sb.append(v).append(' ');
         }
 
         System.out.println(sb);
