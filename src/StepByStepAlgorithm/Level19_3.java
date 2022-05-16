a23370e49b9c0c5ba059f61bb91c6570b3d14ce9
ohjungs <place222@naver.com>
2022-05-16 13:16:43 +0900

    Level19-3

--- src/StepByStepAlgorithm/Level19_3.java
+++ src/StepByStepAlgorithm/Level19_3.java
@@ -23,35 +23,29 @@ public class Level19_3 {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        Queue<Integer> q = new LinkedList<>();
+        LinkedList<Integer> list = new LinkedList<Integer>();
 
         int N = in.nextInt();
         int K = in.nextInt();
 
         for (int i = 1; i <= N; i++) {
-            q.add(i);
+            list.add(i);
         }
 
         StringBuilder sb = new StringBuilder();
         sb.append('<');
 
-        /*
-         * 마지막 부분의 출력은 > 괄호 전에 공백이 없기 때문에
-         * 일괄적으로 출력하기 위해 마지막 원소만 남겨질 때까지만
-         * 반복하고 마지막 원소는 그대로 출력한다.
-         */
-        while (q.size() > 1) {
+        int index = 0; // 리스트에서 삭제할 요소를 참조할 인덱스 변수
 
-            for (int i = 0; i < K - 1; i++) {
-                int val = q.poll();
-                q.offer(val);
-            }
+        while (list.size() > 1) {
+            index = (index + (K - 1)) % list.size();
 
-            sb.append(q.poll()).append(", ");
+            // index위치에 있는 요소를 삭제함과 동시에 출력
+            sb.append(list.remove(index)).append(", ");
         }
 
-        // 마지막 원소 출력한 뒤 > 도 붙여준다.
-        sb.append(q.poll()).append('>');
+        // 마지막으로 남은 요소 삭제함과 동시에 출력
+        sb.append(list.remove()).append('>');
         System.out.println(sb);
     }
 
