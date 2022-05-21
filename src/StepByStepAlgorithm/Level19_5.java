d701108c758ef9d019e215afd7de925bf3d0fc00
ohjungs <place222@naver.com>
2022-05-22 01:06:52 +0900

    Level19-5

--- src/StepByStepAlgorithm/Level19_5.java
+++ src/StepByStepAlgorithm/Level19_5.java
@@ -25,83 +25,146 @@ public class Level19_5 {
 
     // 출력
     // 출력해야하는 명령이 주어질 때마다, 한 줄에 하나씩 출력한다.
+    static int front = 10000;
+    static int back = 10000;
+    static int size = 0;
+    static int[] deque = new int[20001];
+
+    /*
+     * Deque 배열에서 front가 최종적으로 가리키는 위치는 항상 비워둔다.
+     * 즉, 가장 앞에있는 원소는 front + 1이 된다.
+     * 
+     * 이유는 만약 front의 최종 위치에 원소를 넣게 되면 다음과 같
+     * 예외가 발생한다.
+     * 
+     * 예로들어 push_front(3) 을 실행하려 하는데 아무 원소도 없을 때
+     * front--; 감소시킨 뒤 deque[front] = 3; 이 되면
+     * deque[9999] = 3; 이 된다. 이때 front = 9999; back = 10000 이 된다.
+     * 
+     * 하지만, 원소가 한 개만 있을 경우 해당 원소는 front 이자 back 원소가 된다.
+     * 이러한 경우를 방지하기 위해
+     * deque[front] 에 원소를 넣은 뒤, front--; 을 해준다.
+     * 
+     * 결과적으로 front 요소 자체는 deque[front + 1] 위치에 자리한다.
+     */
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        ArrayDeque<Integer> dq = new ArrayDeque<Integer>();
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
-
         for (int i = 0; i < N; i++) {
-
             String[] s = br.readLine().split(" ");
 
-            // 첫 번째 단어에 대한 switch-case
             switch (s[0]) {
 
                 case "push_front": {
-                    dq.addFirst(Integer.parseInt(s[1]));
+                    push_front(Integer.parseInt(s[1]));
                     break;
                 }
 
                 case "push_back": {
-                    dq.addLast(Integer.parseInt(s[1]));
+                    push_back(Integer.parseInt(s[1]));
                     break;
                 }
 
                 case "pop_front": {
-                    if (dq.isEmpty()) {
-                        sb.append(-1).append('\n');
-                    } else {
-                        sb.append(dq.pollFirst()).append('\n');
-                    }
+                    sb.append(pop_front()).append('\n');
                     break;
                 }
 
                 case "pop_back": {
-                    if (dq.isEmpty()) {
-                        sb.append(-1).append('\n');
-                    } else {
-                        sb.append(dq.pollLast()).append('\n');
-                    }
+                    sb.append(pop_back()).append('\n');
                     break;
                 }
 
                 case "size": {
-                    sb.append(dq.size()).append('\n');
+                    sb.append(size()).append('\n');
                     break;
                 }
 
                 case "empty": {
-                    if (dq.isEmpty()) {
-                        sb.append(1).append('\n');
-                    } else {
-                        sb.append(0).append('\n');
-                    }
+                    sb.append(empty()).append('\n');
                     break;
                 }
 
                 case "front": {
-                    if (dq.isEmpty()) {
-                        sb.append(-1).append('\n');
-                    } else {
-                        sb.append(dq.peekFirst()).append('\n');
-                    }
+                    sb.append(front()).append('\n');
                     break;
                 }
 
                 case "back": {
-                    if (dq.isEmpty()) {
-                        sb.append(-1).append('\n');
-                    } else {
-                        sb.append(dq.peekLast()).append('\n');
-                    }
+                    sb.append(back()).append('\n');
                     break;
                 }
             }
+
         }
         System.out.println(sb);
     }
+
+    static void push_front(int val) {
+        // 원소를 먼저 넣은 뒤 front을 감소시킨다.
+        deque[front] = val;
+        front--;
+        size++;
+    }
+
+    static void push_back(int val) {
+
+        back++;
+        size++;
+        deque[back] = val;
+    }
+
+    static int pop_front() {
+        if (size == 0) {
+            return -1;
+        }
+        /*
+         * front + 1이 front 원소이므로 해당 원소를 임시 변수에 둔 뒤
+         * front 을 +1 증가시킨다.
+         */
+        int ret = deque[front + 1];
+        front++;
+        size--;
+        return ret;
+    }
+
+    static int pop_back() {
+        if (size == 0) {
+            return -1;
+        }
+        int ret = deque[back];
+        back--;
+        size--;
+        return ret;
+    }
+
+    static int size() {
+        return size;
+    }
+
+    static int empty() {
+        if (size == 0) {
+            return 1;
+        }
+        return 0;
+    }
+
+    static int front() {
+        if (size == 0) {
+            return -1;
+        }
+        return deque[front + 1];
+    }
+
+    static int back() {
+        if (size == 0) {
+            return -1;
+        }
+        return deque[back];
+    }
+
 }
