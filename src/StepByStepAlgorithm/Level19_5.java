6f13430a803d0e1a148e1ac49572f6b5df69b304
ohjungs <place222@naver.com>
2022-05-21 19:53:50 +0900

    Level19-5

--- src/StepByStepAlgorithm/Level19_5.java
+++ src/StepByStepAlgorithm/Level19_5.java
@@ -1,5 +1,10 @@
 package StepByStepAlgorithm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+
 public class Level19_5 {
     // 10866 문제
     // 정수를 저장하는 덱(Deque)를 구현한 다음, 입력으로 주어지는 명령을 처리하는 프로그램을 작성하시오.
@@ -20,4 +25,83 @@ public class Level19_5 {
 
     // 출력
     // 출력해야하는 명령이 주어질 때마다, 한 줄에 하나씩 출력한다.
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayDeque<Integer> dq = new ArrayDeque<Integer>();
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+
+            String[] s = br.readLine().split(" ");
+
+            // 첫 번째 단어에 대한 switch-case
+            switch (s[0]) {
+
+                case "push_front": {
+                    dq.addFirst(Integer.parseInt(s[1]));
+                    break;
+                }
+
+                case "push_back": {
+                    dq.addLast(Integer.parseInt(s[1]));
+                    break;
+                }
+
+                case "pop_front": {
+                    if (dq.isEmpty()) {
+                        sb.append(-1).append('\n');
+                    } else {
+                        sb.append(dq.pollFirst()).append('\n');
+                    }
+                    break;
+                }
+
+                case "pop_back": {
+                    if (dq.isEmpty()) {
+                        sb.append(-1).append('\n');
+                    } else {
+                        sb.append(dq.pollLast()).append('\n');
+                    }
+                    break;
+                }
+
+                case "size": {
+                    sb.append(dq.size()).append('\n');
+                    break;
+                }
+
+                case "empty": {
+                    if (dq.isEmpty()) {
+                        sb.append(1).append('\n');
+                    } else {
+                        sb.append(0).append('\n');
+                    }
+                    break;
+                }
+
+                case "front": {
+                    if (dq.isEmpty()) {
+                        sb.append(-1).append('\n');
+                    } else {
+                        sb.append(dq.peekFirst()).append('\n');
+                    }
+                    break;
+                }
+
+                case "back": {
+                    if (dq.isEmpty()) {
+                        sb.append(-1).append('\n');
+                    } else {
+                        sb.append(dq.peekLast()).append('\n');
+                    }
+                    break;
+                }
+            }
+        }
+        System.out.println(sb);
+    }
 }
