2beb03b87bf7a965082202e3ad833aefe21c1d1d
ohjungs <place222@naver.com>
2022-05-26 12:27:22 +0900

    Level20-1

--- src/StepByStepAlgorithm/Level20_1.java
+++ src/StepByStepAlgorithm/Level20_1.java
@@ -1,6 +1,10 @@
 package StepByStepAlgorithm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Level20_1 {
 
@@ -26,21 +30,25 @@ l
     // 첫째 줄에는 전체 종이의 한 변의 길이 N이 주어져 있다. N은 2, 4, 8, 16, 32, 64, 128 중 하나이다. 색종이의 각
     // 가로줄의 정사각형칸들의 색이 윗줄부터 차례로 둘째 줄부터 마지막 줄까지 주어진다. 하얀색으로 칠해진 칸은 0, 파란색으로 칠해진 칸은 1로
     // 주어지며, 각 숫자 사이에는 빈칸이 하나씩 있다.
-    public static int white = 0;
+	public static int white = 0;
 	public static int blue = 0;
 	public static int[][] board;
  
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
-		Scanner in = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int N = in.nextInt();
+		int N = Integer.parseInt(br.readLine());
 		
 		board = new int[N][N];
 		
+		StringTokenizer st;
+		
 		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			
 			for(int j = 0; j < N; j++) {
-				board[i][j] = in.nextInt();
+				board[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
