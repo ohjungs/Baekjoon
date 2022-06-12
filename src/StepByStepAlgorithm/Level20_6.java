0bde1193be57223ec8f132244159a2b6dac601a5
ohjungs <place222@naver.com>
2022-06-12 23:30:31 +0900

    Level20-6

--- src/StepByStepAlgorithm/Level20_6.java
+++ src/StepByStepAlgorithm/Level20_6.java
@@ -1,5 +1,10 @@
 package StepByStepAlgorithm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Level20_6 {
     // 2740 행렬곱셈 문제
     // N*M크기의 행렬 A와 M*K크기의 행렬 B가 주어졌을 때, 두 행렬을 곱하는 프로그램을 작성하시오.
@@ -11,4 +16,54 @@ public class Level20_6 {
 
     // 출력
     // 첫째 줄부터 N개의 줄에 행렬 A와 B를 곱한 행렬을 출력한다. 행렬의 각 원소는 공백으로 구분한다.
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        // A행렬 입력
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] A = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < M; j++) {
+                A[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        st = new StringTokenizer(br.readLine(), " ");
+
+        // B행렬 입력
+        st.nextToken(); // 어차피 M값으로 같은 수이기 때문에 버려도 상관 없다.
+        int K = Integer.parseInt(st.nextToken());
+
+        int[][] B = new int[M][K];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < K; j++) {
+                B[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        // 행렬 계산 및 출력
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < K; j++) {
+                int sum = 0;
+                for (int k = 0; k < M; k++) {
+                    sum += A[i][k] * B[k][j];
+                }
+                // A의 i행의 j열 연산이 끝나면 바로 출력문으로 보내준다.
+                sb.append(sum).append(' ');
+            }
+            sb.append('\n');
+        }
+        System.out.println(sb);
+    }
 }
