d082041d7c99681ae0c8c137405c5d3138ef7eb6
ohjungs <place222@naver.com>
2022-07-08 00:23:12 +0900

    Level22-1

--- src/StepByStepAlgorithm/Level22_1.java
+++ src/StepByStepAlgorithm/Level22_1.java
@@ -1,5 +1,8 @@
 package StepByStepAlgorithm;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Level22_1 {
     // 1920 문제
     // N개의 정수 A[1], A[2], …, A[N]이 주어져 있을 때, 이 안에 X라는 정수가 존재하는지 알아내는 프로그램을 작성하시오.
@@ -11,4 +14,67 @@ public class Level22_1 {
 
     // 출력
     // M개의 줄에 답을 출력한다. 존재하면 1을, 존재하지 않으면 0을 출력한다.
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        // 배열은 반드시 정렬되어있어야한다.
+        Arrays.sort(arr);
+
+        int M = in.nextInt();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+
+            // 찾고자 하는 값이 있을 경우 1, 없을 경우 0을 출력해야한다.
+            if (binarySearch(arr, in.nextInt()) >= 0) {
+                sb.append(1).append('\n');
+            } else {
+                sb.append(0).append('\n');
+            }
+        }
+        System.out.println(sb);
+    }
+
+    /**
+     * @param arr 정렬 된 배열
+     * @param key 찾으려는 값
+     * @return key와 일치하는 배열의 인덱스
+     */
+    public static int binarySearch(int[] arr, int key) {
+
+        int lo = 0; // 탐색 범위의 왼쪽 끝 인덱스
+        int hi = arr.length - 1; // 탐색 범위의 오른쪽 끝 인덱스
+
+        // lo가 hi보다 커지기 전까지 반복한다.
+        while (lo <= hi) {
+
+            int mid = (lo + hi) / 2; // 중간위치를 구한다.
+
+            // key값이 중간 위치의 값보다 작을 경우
+            if (key < arr[mid]) {
+                hi = mid - 1;
+            }
+            // key값이 중간 위치의 값보다 클 경우
+            else if (key > arr[mid]) {
+                lo = mid + 1;
+            }
+            // key값과 중간 위치의 값이 같을 경우
+            else {
+                return mid;
+            }
+        }
+
+        // 찾고자 하는 값이 존재하지 않을 경우
+        return -1;
+
+    }
 }
