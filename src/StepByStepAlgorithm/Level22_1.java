090769c1cced70e0a33728735569a392b5ff8f5f
ohjungs <place222@naver.com>
2022-07-10 11:32:56 +0900

    Level22-1

--- src/StepByStepAlgorithm/Level22_1.java
+++ src/StepByStepAlgorithm/Level22_1.java
@@ -1,7 +1,11 @@
 package StepByStepAlgorithm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Level22_1 {
     // 1920 문제
@@ -15,27 +19,32 @@ public class Level22_1 {
     // 출력
     // M개의 줄에 답을 출력한다. 존재하면 1을, 존재하지 않으면 0을 출력한다.
 
-    public static void main(String[] args) {
+    public static int[] arr;
 
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
 
-        int N = in.nextInt();
-        int[] arr = new int[N];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N; i++) {
-            arr[i] = in.nextInt();
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
         // 배열은 반드시 정렬되어있어야한다.
         Arrays.sort(arr);
 
-        int M = in.nextInt();
+        int M = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine(), " ");
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M; i++) {
 
             // 찾고자 하는 값이 있을 경우 1, 없을 경우 0을 출력해야한다.
-            if (binarySearch(arr, in.nextInt()) >= 0) {
+            if (binarySearch(Integer.parseInt(st.nextToken())) >= 0) {
                 sb.append(1).append('\n');
             } else {
                 sb.append(0).append('\n');
@@ -45,11 +54,10 @@ public class Level22_1 {
     }
 
     /**
-     * @param arr 정렬 된 배열
      * @param key 찾으려는 값
      * @return key와 일치하는 배열의 인덱스
      */
-    public static int binarySearch(int[] arr, int key) {
+    public static int binarySearch(int key) {
 
         int lo = 0; // 탐색 범위의 왼쪽 끝 인덱스
         int hi = arr.length - 1; // 탐색 범위의 오른쪽 끝 인덱스
