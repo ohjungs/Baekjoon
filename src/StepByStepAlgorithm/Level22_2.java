859b53dec2898996a0bdd1b2ee7ef9554b79e866
ohjungs <place222@naver.com>
2022-07-11 23:52:09 +0900

    Level22-2

---
+++ src/StepByStepAlgorithm/Level22_2.java
@@ -0,0 +1,95 @@
+package StepByStepAlgorithm;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Level22_2 {
+    // 10816 문제
+    // 숫자 카드는 정수 하나가 적혀져 있는 카드이다. 상근이는 숫자 카드 N개를 가지고 있다. 정수 M개가 주어졌을 때, 이 수가 적혀있는 숫자
+    // 카드를 상근이가 몇 개 가지고 있는지 구하는 프로그램을 작성하시오.
+
+    // 입력
+    // 첫째 줄에 상근이가 가지고 있는 숫자 카드의 개수 N(1 ≤ N ≤ 500,000)이 주어진다. 둘째 줄에는 숫자 카드에 적혀있는 정수가
+    // 주어진다. 숫자 카드에 적혀있는 수는 -10,000,000보다 크거나 같고, 10,000,000보다 작거나 같다.
+
+    // 셋째 줄에는 M(1 ≤ M ≤ 500,000)이 주어진다. 넷째 줄에는 상근이가 몇 개 가지고 있는 숫자 카드인지 구해야 할 M개의 정수가
+    // 주어지며, 이 수는 공백으로 구분되어져 있다. 이 수도 -10,000,000보다 크거나 같고, 10,000,000보다 작거나 같다.
+
+    // 출력
+    // 첫째 줄에 입력으로 주어진 M개의 수에 대해서, 각 수가 적힌 숫자 카드를 상근이가 몇 개 가지고 있는지를 공백으로 구분해 출력한다.
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        Arrays.sort(arr); // 이분 탐색을 하기 위해서는 반드시 정렬되어있어야 한다.
+
+        int M = in.nextInt();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < M; i++) {
+            int key = in.nextInt();
+
+            // upperBound와 lowerBound의 차이 값을 구한다.
+            sb.append(upperBound(arr, key) - lowerBound(arr, key)).append(' ');
+        }
+        System.out.println(sb);
+    }
+
+    private static int lowerBound(int[] arr, int key) {
+        int lo = 0;
+        int hi = arr.length;
+
+        // lo가 hi랑 같아질 때 까지 반복
+        while (lo < hi) {
+
+            int mid = (lo + hi) / 2; // 중간위치를 구한다.
+
+            /*
+             * key 값이 중간 위치의 값보다 작거나 같을 경우
+             * 
+             * (중복 원소에 대해 왼쪽으로 탐색하도록 상계를 내린다.)
+             */
+            if (key <= arr[mid]) {
+                hi = mid;
+            }
+
+            else {
+                lo = mid + 1;
+            }
+
+        }
+
+        return lo;
+    }
+
+    private static int upperBound(int[] arr, int key) {
+        int lo = 0;
+        int hi = arr.length;
+
+        // lo가 hi랑 같아질 때 까지 반복
+        while (lo < hi) {
+
+            int mid = (lo + hi) / 2; // 중간위치를 구한다.
+
+            // key값이 중간 위치의 값보다 작을 경우
+            if (key < arr[mid]) {
+                hi = mid;
+            }
+            // 중복원소의 경우 else에서 처리된다.
+            else {
+                lo = mid + 1;
+            }
+
+        }
+
+        return lo;
+    }
+}
