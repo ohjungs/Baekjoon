be3e6c478b71508879eb3af0072a42011aa73ca8
ohjungs <place222@naver.com>
2022-07-14 00:19:34 +0900

    Level22-2

--- src/StepByStepAlgorithm/Level22_2.java
+++ src/StepByStepAlgorithm/Level22_2.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
@@ -25,77 +26,39 @@ public class Level22_2 {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        /*
+         * HashMap<Key, Value>
+         * Key = 입력되는 원소
+         * Value = 원소의 개수(=중복 입력 된 원소의 수)
+         */
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
 
-        Arrays.sort(arr);
+        for (int i = 0; i < N; i++) {
+            int key = Integer.parseInt(st.nextToken());
+
+            /*
+             * getOrDefault(key, defaultValue)
+             * key에 대해 map에 저장 된 value를 반환한다.
+             * 만약 value가 없을 경우 defaultValue값을 반환한다.
+             */
+            map.put(key, map.getOrDefault(key, 0) + 1);
+        }
 
         int M = Integer.parseInt(br.readLine());
 
-        st = new StringTokenizer(br.readLine(), " ");
         StringBuilder sb = new StringBuilder();
 
+        st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < M; i++) {
             int key = Integer.parseInt(st.nextToken());
 
-            // upperBound와 lowerBound의 차이 값을 구한다.
-            sb.append(upperBound(arr, key) - lowerBound(arr, key)).append(' ');
+            sb.append(map.getOrDefault(key, 0)).append(' ');
         }
+
         System.out.println(sb);
-    }
-
-    private static int lowerBound(int[] arr, int key) {
-        int lo = 0;
-        int hi = arr.length;
-
-        // lo가 hi랑 같아질 때 까지 반복
-        while (lo < hi) {
-
-            int mid = (lo + hi) / 2; // 중간위치를 구한다.
-
-            /*
-             * key 값이 중간 위치의 값보다 작거나 같을 경우
-             * 
-             * (중복 원소에 대해 왼쪽으로 탐색하도록 상계를 내린다.)
-             */
-            if (key <= arr[mid]) {
-                hi = mid;
-            }
-
-            else {
-                lo = mid + 1;
-            }
-
-        }
-
-        return lo;
-    }
-
-    private static int upperBound(int[] arr, int key) {
-        int lo = 0;
-        int hi = arr.length;
-
-        // lo가 hi랑 같아질 때 까지 반복
-        while (lo < hi) {
-
-            int mid = (lo + hi) / 2; // 중간위치를 구한다.
-
-            // key값이 중간 위치의 값보다 작을 경우
-            if (key < arr[mid]) {
-                hi = mid;
-            }
-            // 중복원소의 경우 else에서 처리된다.
-            else {
-                lo = mid + 1;
-            }
-
-        }
-
-        return lo;
     }
 }
