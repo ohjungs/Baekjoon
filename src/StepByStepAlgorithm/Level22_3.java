9d4d0733f3f0a16e14a48f7f7abb5667595e8364
ohjungs <place222@naver.com>
2022-07-17 16:45:11 +0900

    Level22-3

--- src/StepByStepAlgorithm/Level22_3.java
+++ src/StepByStepAlgorithm/Level22_3.java
@@ -1,5 +1,7 @@
 package StepByStepAlgorithm;
 
+import java.util.Scanner;
+
 public class Level22_3 {
     // 1654 랜선 자르기 문제
     // 집에서 시간을 보내던 오영식은 박성원의 부름을 받고 급히 달려왔다. 박성원이 캠프 때 쓸 N개의 랜선을 만들어야 하는데 너무 바빠서
@@ -20,4 +22,59 @@ public class Level22_3 {
 
     // 출력
     // 첫째 줄에 N개를 만들 수 있는 랜선의 최대 길이를 센티미터 단위의 정수로 출력한다.
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int K = in.nextInt();
+        int N = in.nextInt();
+
+        int[] arr = new int[K];
+
+        long max = 0;
+
+        // 입력과 동시에 해당 랜선의 길이가 최댓값인지를 확인하고 max를 갱신
+        for (int i = 0; i < K; i++) {
+            arr[i] = in.nextInt();
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+
+        // 반드시 max에서 +1 값이어야 한다.
+        max++;
+
+        long min = 0; // 탐색 길이 최솟값
+        long mid = 0;
+
+        while (min < max) {
+
+            // 범위 내에서 중간 길이를 구한다.
+            mid = (max + min) / 2;
+
+            long count = 0;
+
+            // 구해진 중간 길이로 잘라서 총 몇 개가 만들어지는지를 구한다.
+
+            for (int i = 0; i < arr.length; i++) {
+                count += (arr[i] / mid);
+            }
+
+            /*
+             * [upper bound 형식]
+             * 
+             * mid길이로 잘랐을 때의 개수가 만들고자 하는 랜선의 개수보다 작다면
+             * 자르고자 하는 길이를 줄이기 위해 최대 길이를 줄인다.
+             * 그 외에는 자르고자 하는 길이를 늘려야 하므로 최소 길이를 늘린다.
+             */
+            if (count < N) {
+                max = mid;
+            } else {
+                min = mid + 1;
+            }
+
+        }
+
+        // UpperBound로 얻어진 값(min)에 -1이 최대 길이가 된다.
+        System.out.println(min - 1);
+    }
 }
