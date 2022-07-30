8fb18d3ba56a5a90f5a92ce9857dc2e9f4a41dc4
oh <place222@naver.com>
2022-07-30 12:48:43 +0900

    Level22-6

--- src/StepByStepAlgorithm/Level22_6.java
+++ src/StepByStepAlgorithm/Level22_6.java
@@ -1,5 +1,8 @@
 package StepByStepAlgorithm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Level22_6 {
@@ -15,12 +18,12 @@ public class Level22_6 {
 
     // 출력
     // B[k]를 출력한다.
-    public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
 
-        int N = in.nextInt();
-        int K = in.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int K = Integer.parseInt(br.readLine());
 
         // x는 lo <= x <= hi 의 범위를 갖는다.
         long lo = 1;
