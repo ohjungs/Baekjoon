51f9fd226a9a5d3c8a1778520db73a11054aa26a
oh <place222@naver.com>
2022-08-26 08:58:44 +0900

    Level24-5

--- src/StepByStepAlgorithm/Level24_5.java
+++ src/StepByStepAlgorithm/Level24_5.java
@@ -1,5 +1,9 @@
 package StepByStepAlgorithm;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Level24_5 {
     // 2629 양팔저울 문제
     // 양팔 저울과 몇 개의 추가 주어졌을 때, 이를 이용하여 입력으로 주어진 구슬의 무게를 확인할 수 있는지를 결정하려고 한다.
@@ -30,5 +34,59 @@ public class Level24_5 {
     // 출력
     // 주어진 각 구슬의 무게에 대하여 확인이 가능하면 Y, 아니면 N 을 차례로 출력한다. 출력은 한 개의 줄로 이루어지며, 각 구슬에 대한 답
     // 사이에는 빈칸을 하나씩 둔다.
+    static int n;
+    static int[] w;
+    static boolean[][] result;
 
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        w = new int[n];
+        result = new boolean[n + 1][40001];
+        for (int i = 0; i < n; i++) {
+            int src = Integer.parseInt(st.nextToken());
+            w[i] = src;
+        }
+
+        dp(0, 0);
+
+        for (int j = 0; j < 20; j++) {
+            if (result[n][j]) {
+                System.out.println(j);
+                System.out.print(result[n][j] + " ");
+            }
+        }
+
+        int c = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < c; i++) {
+            int t = Integer.parseInt(st.nextToken());
+
+            if (result[n][t]) {
+                sb.append("Y ");
+            } else {
+                sb.append("N ");
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    static void dp(int cnt, int num) {
+        if (result[cnt][num])
+            return;
+        result[cnt][num] = true;
+
+        if (cnt == n)
+            return;
+
+        dp(cnt + 1, num + w[cnt]);
+        dp(cnt + 1, num);
+        dp(cnt + 1, Math.abs(num - w[cnt]));
+
+    }
 }
