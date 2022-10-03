ba4dbdddbc1f303a795ffc796ea2ff33414656a5
oh <place222@naver.com>
2022-10-03 20:42:16 +0900

    Level25-4

--- src/StepByStepAlgorithm/Level25_4.java
+++ src/StepByStepAlgorithm/Level25_4.java
@@ -24,46 +24,38 @@ public class Level25_4 {
     // 출력
     // 1번 컴퓨터가 웜 바이러스에 걸렸을 때, 1번 컴퓨터를 통해 웜 바이러스에 걸리게 되는 컴퓨터의 수를 첫째 줄에 출력한다.
 
-    static int node[][]; // 그래프 배열
-    static int check[]; // 방문 배열
+    static int map[][];
+    static boolean visit[];
+    static int n, m, v;
+    static int count = 0;
 
-    static void bfs(int start) { // BFS 메소드
-        Queue<Integer> queue = new LinkedList<>();
+    public static int dfs(int i) {
+        visit[i] = true;
 
-        check[start] = 1;
-        queue.offer(start);
-        int cnt = 0; // 감염 된 컴퓨터의 수
-        while (!queue.isEmpty()) {
-            int x = queue.poll();
-
-            for (int i = 1; i < node.length; i++) { // 차례대로 1번과 연결 된 컴퓨터들을 찾아 cnt변수 증가
-                if (node[x][i] == 1 && check[i] != 1) {
-                    queue.offer(i);
-                    check[i] = 1;
-                    cnt++;
-                }
+        for (int j = 1; j <= n; j++) {
+            if (map[i][j] == 1 && visit[j] == false) {
+                count++;
+                dfs(j);
             }
         }
-        System.out.println(cnt); // 모든 탐색을 마치고 cnt 출력
+        return count;
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
+        n = scan.nextInt(); // 컴퓨터 수(정점)
+        m = scan.nextInt(); // 연결된 컴퓨터 쌍의 수(간선)
+        v = 1; // 탐색 시장할 정점의 번호
+        map = new int[n + 1][n + 1]; // 각 정점간 탐색 경로를 저장할 배열
+        visit = new boolean[n + 1]; // 정점의 탐색 여부 체크
 
-        int n = sc.nextInt(); // 컴퓨터의 수
-        int m = sc.nextInt(); // 네트워크 상에 연결되어 있는 컴퓨터 쌍의 수 즉, 간선의 수
-
-        node = new int[n + 1][n + 1];
-        check = new int[n + 1];
-        for (int i = 0; i < m; i++) { // 그래프 구성
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            node[a][b] = 1;
-            node[b][a] = 1;
-
+        for (int i = 0; i < m; i++) {
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            map[a][b] = map[b][a] = 1;
         }
 
-        bfs(1);
-
+        System.out.println(dfs(1));
+        scan.close();
     }
 }
