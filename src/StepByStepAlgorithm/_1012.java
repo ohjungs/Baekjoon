d445cfca9593e4f3dcdaec7c83ba100e55e4fd3f
oh <place222@naver.com>
2022-10-20 21:08:26 +0900

    1012

--- src/StepByStepAlgorithm/_1012.java
+++ src/StepByStepAlgorithm/_1012.java
@@ -1,5 +1,10 @@
 package StepByStepAlgorithm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class _1012 {
     // 유기농 배추 문제
     // 차세대 영농인 한나는 강원도 고랭지에서 유기농 배추를 재배하기로 하였다. 농약을 쓰지 않고 배추를 재배하려면 배추를 해충으로부터 보호하는
@@ -25,4 +30,62 @@ public class _1012 {
 
     // 출력
     // 각 테스트 케이스에 대해 필요한 최소의 배추흰지렁이 마리 수를 출력한다.
+    static int M, N, K;
+    static int[][] cabbage;
+    static boolean[][] visit;
+    static int count;
+    static int[] dx = { 0, -1, 0, 1 };
+    static int[] dy = { 1, 0, -1, 0 };
+
+    static void dfs(int x, int y) {
+        visit[x][y] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int cx = x + dx[i];
+            int cy = y + dy[i];
+
+            if (cx >= 0 && cy >= 0 && cx < M && cy < N) {
+                if (!visit[cx][cy] && cabbage[cx][cy] == 1) {
+                    dfs(cx, cy);
+                }
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < tc; i++) {
+            count = 0;
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            cabbage = new int[M][N];
+            visit = new boolean[M][N];
+
+            K = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < K; j++) {
+                st = new StringTokenizer(br.readLine(), " ");
+                int p1 = Integer.parseInt(st.nextToken());
+                int p2 = Integer.parseInt(st.nextToken());
+                cabbage[p1][p2] = 1;
+            }
+
+            for (int x = 0; x < M; x++) {
+                for (int y = 0; y < N; y++) {
+                    if (cabbage[x][y] == 1 && !visit[x][y]) {
+                        dfs(x, y);
+                        count++;
+                    }
+                }
+            }
+
+            System.out.println(count);
+        }
+
+    }
 }
