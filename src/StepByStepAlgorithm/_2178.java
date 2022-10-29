dccf59188af817caef75afc713dee28c0ffa4528
oh <place222@naver.com>
2022-10-29 12:38:56 +0900

    2178

--- src/StepByStepAlgorithm/_2178.java
+++ src/StepByStepAlgorithm/_2178.java
@@ -1,5 +1,12 @@
 package StepByStepAlgorithm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
 public class _2178 {
     // N×M크기의 배열로 표현되는 미로가 있다.
 
@@ -19,4 +26,55 @@ public class _2178 {
 
     // 출력
     // 첫째 줄에 지나야 하는 최소의 칸 수를 출력한다. 항상 도착위치로 이동할 수 있는 경우만 입력으로 주어진다.
+    static int[][] map;
+    static int n;
+    static int m;
+    static boolean[][] visited;
+    static int[] dx = { -1, 1, 0, 0 }; // x방향배열-상하
+    static int[] dy = { 0, 0, -1, 1 }; // y방향배열-좌우
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        map = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < m; j++) {
+                map[i][j] = s.charAt(j) - '0';
+            }
+        }
+
+        visited = new boolean[n][m];
+        visited[0][0] = true;
+        bfs(0, 0);
+        System.out.println(map[n - 1][m - 1]);
+    }
+
+    public static void bfs(int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[] { x, y });
+
+        while (!q.isEmpty()) {
+            int now[] = q.poll();
+            int nowX = now[0];
+            int nowY = now[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nextX = nowX + dx[i];
+                int nextY = nowY + dy[i];
+
+                if (nextX < 0 || nextY < 0 || nextX >= n || nextY >= m)
+                    continue;
+                if (visited[nextX][nextY] || map[nextX][nextY] == 0)
+                    continue;
+
+                q.add(new int[] { nextX, nextY });
+                map[nextX][nextY] = map[nowX][nowY] + 1;
+                visited[nextX][nextY] = true;
+            }
+        }
+    }
 }
