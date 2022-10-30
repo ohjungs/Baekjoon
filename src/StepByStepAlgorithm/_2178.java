c6fcfc8118dac1b8a6a401d103a27689e54e413d
oh <place222@naver.com>
2022-10-30 12:50:59 +0900

    2178

--- src/StepByStepAlgorithm/_2178.java
+++ src/StepByStepAlgorithm/_2178.java
@@ -26,12 +26,12 @@ public class _2178 {
 
     // 출력
     // 첫째 줄에 지나야 하는 최소의 칸 수를 출력한다. 항상 도착위치로 이동할 수 있는 경우만 입력으로 주어진다.
+
     static int[][] map;
     static int n;
     static int m;
+    static int minVal;
     static boolean[][] visited;
-    static int[] dx = { -1, 1, 0, 0 }; // x방향배열-상하
-    static int[] dy = { 0, 0, -1, 1 }; // y방향배열-좌우
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -49,32 +49,40 @@ public class _2178 {
 
         visited = new boolean[n][m];
         visited[0][0] = true;
-        bfs(0, 0);
-        System.out.println(map[n - 1][m - 1]);
+        minVal = Integer.MAX_VALUE;
+        dfs(0, 0, 1);
+        System.out.println(minVal);
     }
 
-    public static void bfs(int x, int y) {
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[] { x, y });
+    public static void dfs(int x, int y, int count) {
+        if (x == n - 1 && y == m - 1) {
+            minVal = Math.min(minVal, count);
+            return;
+        }
 
-        while (!q.isEmpty()) {
-            int now[] = q.poll();
-            int nowX = now[0];
-            int nowY = now[1];
+        if (count > minVal)
+            return; // 가지치기 - 이미 count가 minVal보다 커졌다면 return;
 
-            for (int i = 0; i < 4; i++) {
-                int nextX = nowX + dx[i];
-                int nextY = nowY + dy[i];
-
-                if (nextX < 0 || nextY < 0 || nextX >= n || nextY >= m)
-                    continue;
-                if (visited[nextX][nextY] || map[nextX][nextY] == 0)
-                    continue;
-
-                q.add(new int[] { nextX, nextY });
-                map[nextX][nextY] = map[nowX][nowY] + 1;
-                visited[nextX][nextY] = true;
-            }
+        // 방향배열 사용하지 않고 조건문으로 4가지를 나누어 보았다.
+        if (x > 0 && !visited[x - 1][y] && map[x - 1][y] == 1) { // 상
+            visited[x - 1][y] = true;
+            dfs(x - 1, y, count + 1);
+            visited[x - 1][y] = false;
+        }
+        if (x < n - 1 && !visited[x + 1][y] && map[x + 1][y] == 1) { // 하
+            visited[x + 1][y] = true;
+            dfs(x + 1, y, count + 1);
+            visited[x + 1][y] = false;
+        }
+        if (y > 0 && !visited[x][y - 1] && map[x][y - 1] == 1) { // 좌
+            visited[x][y - 1] = true;
+            dfs(x, y - 1, count + 1);
+            visited[x][y - 1] = false;
+        }
+        if (y < m - 1 && !visited[x][y + 1] && map[x][y + 1] == 1) { // 우
+            visited[x][y + 1] = true;
+            dfs(x, y + 1, count + 1);
+            visited[x][y + 1] = false;
         }
     }
 }
